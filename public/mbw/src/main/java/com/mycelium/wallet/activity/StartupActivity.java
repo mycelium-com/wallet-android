3663dcd2e30880192446ff1cb3bedd0cc2648406
WACOMalt <wacomalt@gmail.com>
2014-05-11 02:27:09 -0400

    Added support for NFC bitcoin URI handling
    Added NFC to the bitcoin URI intent, and StartupActivity now also checks
    for NFC event.
    NFC bitcoin: URI events should now be handled in entirely the same way
    as a clicked link.

--- public/mbw/src/main/java/com/mycelium/wallet/activity/StartupActivity.java
+++ public/mbw/src/main/java/com/mycelium/wallet/activity/StartupActivity.java
@@ -39,6 +39,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Window;
