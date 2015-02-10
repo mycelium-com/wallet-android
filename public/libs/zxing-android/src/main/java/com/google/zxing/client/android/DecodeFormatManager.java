39d5279966d9d6b4d71566c94adfb30e21ad6221
Jan Dreske <jan.dreske@mycelium.com>
2015-02-10 15:16:09 +0100

    move libs

--- public/libs/zxing-android/src/main/java/com/google/zxing/client/android/DecodeFormatManager.java
+++ public/libs/zxing-android/src/main/java/com/google/zxing/client/android/DecodeFormatManager.java
@@ -23,7 +23,6 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 import android.content.Intent;
-import android.net.Uri;
 import com.google.zxing.BarcodeFormat;
 
 final class DecodeFormatManager {
