bbe3131cc705346835e168cf780207e296ebfbf4
Leo Wandersleb <leo@leowandersleb.de>
2017-06-22 22:07:41 -0400

    made the code more readable
    a static import on R.strings.* is not very common, neither but the changes like
    getString(R.string.partner_ledger_info)
    to
    partner_ledger_info
    makes things a bit more readable.
    I was not so sure about using the static import for R.string.ok though.

--- mbw/src/main/java/com/mycelium/wallet/activity/main/RecommendationsFragment.java
+++ mbw/src/main/java/com/mycelium/wallet/activity/main/RecommendationsFragment.java
@@ -54,44 +54,25 @@ import com.mycelium.wallet.activity.util.RecommendationAdapter;
 
 import java.util.ArrayList;
 
+import static com.mycelium.wallet.R.string.*;
+
 
 public class RecommendationsFragment extends Fragment {
-    private View _root;
-
     ListView recommendationsList;
     TextView moreInformation;
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
-        _root = inflater.inflate(R.layout.main_recommendations_view, container, false);
+        View root = inflater.inflate(R.layout.main_recommendations_view, container, false);
 
-        recommendationsList = (ListView) _root.findViewById(R.id.list);
-        ArrayList list = new ArrayList<PartnerInfo>();
+        recommendationsList = (ListView) root.findViewById(R.id.list);
+        ArrayList<PartnerInfo> list = new ArrayList<>();
 
-        list.add(new PartnerInfo(getString(R.string.partner_ledger),
-                getString(R.string.partner_ledger_short),
-                getString(R.string.partner_ledger_info),
-                getString(R.string.partner_ledger_url), R.drawable.ledger_icon));
-
-        list.add(new PartnerInfo(getString(R.string.partner_trezor),
-                                 getString(R.string.partner_trezor_short),
-                                 getString(R.string.partner_trezor_info),
-                                 getString(R.string.partner_trezor_url), R.drawable.trezor2));
-
-        list.add(new PartnerInfo(getString(R.string.partner_purse),
-                getString(R.string.partner_purse_short),
-                getString(R.string.partner_purse_info),
-                getString(R.string.partner_purse_url), R.drawable.purse_small));
-
-        list.add(new PartnerInfo(getString(R.string.partner_coinbase),
-                getString(R.string.partner_coinbase_short),
-                getString(R.string.partner_coinbase_info),
-                getString(R.string.partner_coinbase_url), R.drawable.coinbase));
-
-        list.add(new PartnerInfo(getString(R.string.partner_hashing24),
-                getString(R.string.partner_hashing24_short),
-                getString(R.string.partner_hashing24_info),
-                getString(R.string.partner_hashing24_url), R.drawable.hashing24));
+        list.add(getPartnerInfo(partner_ledger, partner_ledger_short, partner_ledger_info, partner_ledger_url, R.drawable.ledger_icon));
+        list.add(getPartnerInfo(partner_trezor, partner_trezor_short, partner_trezor_info, partner_trezor_url, R.drawable.trezor2));
+        list.add(getPartnerInfo(partner_purse, partner_purse_short, partner_purse_info, partner_purse_url, R.drawable.purse_small));
+        list.add(getPartnerInfo(partner_coinbase, partner_coinbase_short, partner_coinbase_info, partner_coinbase_url, R.drawable.coinbase));
+        list.add(getPartnerInfo(partner_hashing24, partner_hashing24_short, partner_hashing24_info, partner_hashing24_url, R.drawable.hashing24));
 
         //View footerView = ((LayoutInflater) ActivityContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.footer_layout, null, false);
         View footerView = getActivity().getLayoutInflater().inflate(R.layout.main_recommendations_list_footer, null, false);
@@ -103,17 +84,17 @@ public class RecommendationsFragment extends Fragment {
                 View custom = LayoutInflater.from(view.getContext())
                         .inflate(R.layout.main_recommendation_dialog_view, null, false);
                 TextView part1 = (TextView) custom.findViewById(R.id.part1);
-                part1.setText(R.string.partner_more_info_text_part1);
+                part1.setText(partner_more_info_text_part1);
 
                 TextView part2 = (TextView) custom.findViewById(R.id.part2);
-                part2.setText(R.string.partner_more_info_text_part2);
+                part2.setText(partner_more_info_text_part2);
 
                 ((ImageView) custom.findViewById(R.id.image)).setImageResource(R.drawable.mycelium_logo_transp);
 
                 AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
-                builder.setTitle(R.string.partner_more_info);
-//                builder.setMessage(R.string.partner_more_info_text);
-                builder.setPositiveButton(R.string.ok,
+                builder.setTitle(partner_more_info);
+//                builder.setMessage(partner_more_info_text);
+                builder.setPositiveButton(ok,
                         new DialogInterface.OnClickListener() {
                             @Override public void onClick(DialogInterface dialogInterface, int i) { }
                         } );
@@ -143,10 +124,10 @@ public class RecommendationsFragment extends Fragment {
 
                     AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 //                        builder.setMessage(bean.getInfo());
-                    builder.setTitle(R.string.warning_partner);
+                    builder.setTitle(warning_partner);
 //                        builder.setIcon(bean.getIcon());
                     builder.setView(custom);
-                    builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    builder.setPositiveButton(ok, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             if (bean.getUri() != null) {
                                 Intent intent = new Intent(Intent.ACTION_VIEW);
@@ -155,20 +136,20 @@ public class RecommendationsFragment extends Fragment {
                             }
                         }
                     });
-                    builder.setNegativeButton(R.string.cancel, null);
+                    builder.setNegativeButton(cancel, null);
                     AlertDialog dialog = builder.create();
                     dialog.show();
                 } else {
                     if (bean.getUri() != null) {
                         Intent i = new Intent(Intent.ACTION_VIEW);
-                        i.setData(Uri.parse(bean.getUri().toString()));
+                        i.setData(Uri.parse(bean.getUri()));
                         startActivity(i);
                     }
                 }
             }
         });
 
-        return _root;
+        return root;
     }
 
     @Override
@@ -177,19 +158,7 @@ public class RecommendationsFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
+    private PartnerInfo getPartnerInfo(int name, int description, int disclaimer, int uri, int icon) {
+        return new PartnerInfo(getString(name), getString(description), getString(disclaimer), getString(uri), icon);
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-    }
-
 }
