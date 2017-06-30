d5105697adb356e1ef4a4580e0e659a2776a8e4f
elvis <xelvis89x@gmail.com>
2017-06-30 14:18:59 +0300

    active button rmc reminder dialog
    (cherry picked from commit 3de0363)

--- mbw/src/main/java/com/mycelium/wallet/activity/rmc/RMCAddressFragment.java
+++ mbw/src/main/java/com/mycelium/wallet/activity/rmc/RMCAddressFragment.java
@@ -1,15 +1,15 @@
 package com.mycelium.wallet.activity.rmc;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
 import android.widget.ViewFlipper;
-import android.widget.ViewSwitcher;
 
 import com.google.common.base.Preconditions;
 import com.jjoe64.graphview.GraphView;
@@ -34,6 +34,9 @@ public class RMCAddressFragment extends Fragment {
     @BindView(R.id.graph)
     protected GraphView graphView;
 
+    @BindView(R.id.active_in_day_progress)
+    protected ProgressBar activeProgressBar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         _root = Preconditions.checkNotNull(inflater.inflate(R.layout.rmc_address_view, container, false));
@@ -54,7 +57,8 @@ public class RMCAddressFragment extends Fragment {
                 new DataPoint(4, 6)
         });
         graphView.addSeries(series);
-
+        activeProgressBar.setProgress(20);
+        activeProgressBar.setMax(100);
     }
 
     @OnClick(R.id.show_graph)
@@ -65,5 +69,20 @@ public class RMCAddressFragment extends Fragment {
     @OnClick(R.id.show_stats)
     void clickShowStats() {
         switcher.showPrevious();
+    }
+
+    @OnClick(R.id.rmc_active_set_reminder)
+    void setReminderClick() {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_rmc_reminder, null, false);
+        new AlertDialog.Builder(getActivity()).setTitle("RMC reminder")
+                .setView(view)
+                .setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                }).setNegativeButton("CANCEL", null)
+                .create()
+                .show();
     }
 }
