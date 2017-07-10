815fd61938141e11a3a503930d12b4b628f08ce3
elvis <xElvis89x@gmail.com>
2017-07-10 19:39:45 +0300

    currency triangle

--- mbw/src/main/java/com/mycelium/wallet/activity/util/ToggleableCurrencyButton.java
+++ mbw/src/main/java/com/mycelium/wallet/activity/util/ToggleableCurrencyButton.java
@@ -83,7 +83,8 @@ public class ToggleableCurrencyButton extends ToggleableCurrencyDisplay {
       super.updateUi();
 
       int cntCurrencies = (fiatOnly ? currencySwitcher.getFiatCurrenciesCount() : currencySwitcher.getCurrenciesCount() );
-      if (cntCurrencies == 1){
+      if (cntCurrencies == 1
+              || (currentValue != null && currentValue.getCurrency().equals(ColuAccount.ColuAssetType.RMC.toString()))){
          // there is only one currency to show - dont show a triangle hinting that the user can toggle
          findViewById(R.id.ivSwitchable).setVisibility(INVISIBLE);
       } else {
