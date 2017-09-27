c1c9fd404c686834663d04b31c7fbc7b82ce13c8
elvis <xElvis89x@gmail.com>
2017-09-27 11:12:44 +0300

    verify message

--- mbw/src/main/java/com/mycelium/wallet/activity/MessageVerifyActivity.java
+++ mbw/src/main/java/com/mycelium/wallet/activity/MessageVerifyActivity.java
@@ -5,32 +5,67 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
-import com.mrd.bitlib.crypto.PublicKey;
+import com.mrd.bitlib.crypto.SignedMessage;
+import com.mrd.bitlib.crypto.WrongSignatureException;
+import com.mrd.bitlib.lambdaworks.crypto.Base64;
+import com.mrd.bitlib.model.Address;
+import com.mrd.bitlib.util.ByteWriter;
 import com.mrd.bitlib.util.HashUtils;
 import com.mrd.bitlib.util.Sha256Hash;
+import com.mycelium.wallet.MbwManager;
 import com.mycelium.wallet.R;
+import com.mycelium.wallet.Utils;
+import com.mycelium.wapi.wallet.SecureKeyValueStore;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MessageVerifyActivity extends Activity {
-    private Pattern messagePattern = Pattern.compile("-----BEGIN BITCOIN SIGNED MESSAGE-----" +
-            "(.*?)" +
-            "-----BEGIN BITCOIN SIGNATURE-----" +
-            "Version: (.*?)" +
-            "Address: (.*?)" +
-            "(.*?)" +
+    private static final byte[] HEADER;
+    private static final byte[] SIGNING_HEADER;
+
+    static {
+        try {
+            HEADER = "Bitcoin Signed Message:\n".getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        SIGNING_HEADER = standardSigningHeader();
+    }
+
+    private Pattern messagePattern = Pattern.compile("-----BEGIN BITCOIN SIGNED MESSAGE-----\n?" +
+            "(.*?)\n?" +
+            "-----BEGIN BITCOIN SIGNATURE-----\n?" +
+            "(Version: )?(.*?)\n?" +
+            "(Address: )?(.*?)\n?\n?" +
+            "(.*?)\n?" +
             "-----END BITCOIN SIGNATURE-----");
     private EditText signedMessageEditText;
+    private TextView verifyResultView;
+
+    SecureKeyValueStore store;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_verify);
+        store = MbwManager.getInstance(MessageVerifyActivity.this).getWalletManager(false).getSecureStorage();
         signedMessageEditText = (EditText) findViewById(R.id.signedMessage);
+        verifyResultView = (TextView) findViewById(R.id.verifyResult);
+        findViewById(R.id.btPaste).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String clipboard = Utils.getClipboardString(MessageVerifyActivity.this);
+                signedMessageEditText.setText(clipboard);
+            }
+        });
         signedMessageEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -44,21 +79,49 @@ public class MessageVerifyActivity extends Activity {
 
             @Override
             public void afterTextChanged(Editable editable) {
+                boolean checkResult = false;
                 String msgWithSign = signedMessageEditText.getText().toString();
                 Matcher matcher = messagePattern.matcher(msgWithSign);
-                String address = matcher.group(3);
-                PublicKey publicKey = new PublicKey(address.getBytes());
-                String msg = matcher.group(1);
-                Sha256Hash data = null;
-                try {
-                    data = HashUtils.doubleSha256(msg.getBytes("UTF-8"));
-                } catch (UnsupportedEncodingException e) {
-                    Log.e("", "encoding problem ", e);
-                }
-                String signatire = matcher.group(4);
-                boolean checkResult = publicKey.verifyDerEncodedSignature(data, signatire.getBytes());
+                if (matcher.find()) {
+                    Address address = Address.fromString(matcher.group(4));
+                    String msg = matcher.group(1);
+                    Sha256Hash data = HashUtils.doubleSha256(formatMessageForSigning(msg));
+                    try {
+                        SignedMessage signedMessage = SignedMessage.validate(address, msg, matcher.group(6));
+                        checkResult = signedMessage.getPublicKey().verifyDerEncodedSignature(data, signedMessage.getDerEncodedSignature());
+                    } catch (WrongSignatureException e) {
+                        Log.e("MessageVerifyActivity", "WrongSignatureException", e);
+                    }
 
+                }
+                verifyResultView.setText(checkResult ? "verified" : "failed");
             }
         });
     }
+
+    static byte[] formatMessageForSigning(String message) {
+        byte[] messageBytes;
+        try {
+            messageBytes = message.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        ByteWriter writer = new ByteWriter(messageBytes.length + SIGNING_HEADER.length + 1);
+        writer.putBytes(SIGNING_HEADER);
+        writer.putCompactInt(message.length());
+        writer.putBytes(messageBytes);
+        return writer.toBytes();
+    }
+
+    private static byte[] standardSigningHeader() {
+        ByteArrayOutputStream bos1 = new ByteArrayOutputStream();
+        bos1.write(HEADER.length);
+        try {
+            bos1.write(HEADER);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return bos1.toByteArray();
+    }
+
 }
