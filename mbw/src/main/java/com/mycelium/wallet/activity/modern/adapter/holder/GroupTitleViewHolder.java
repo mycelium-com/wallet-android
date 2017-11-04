14deebd8c30bc39b9b81df4299f5e15a7bf2326e
elvis <xElvis89x@gmail.com>
2017-11-05 00:01:50 +0300

    implement expanding/collapsing account groups

--- mbw/src/main/java/com/mycelium/wallet/activity/modern/adapter/holder/GroupTitleViewHolder.java
+++ mbw/src/main/java/com/mycelium/wallet/activity/modern/adapter/holder/GroupTitleViewHolder.java
@@ -2,6 +2,7 @@ package com.mycelium.wallet.activity.modern.adapter.holder;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mycelium.wallet.R;
@@ -10,11 +11,15 @@ import com.mycelium.wallet.activity.util.ToggleableCurrencyDisplay;
 
 public class GroupTitleViewHolder extends RecyclerView.ViewHolder {
     public TextView tvTitle;
+    public TextView tvAccountsCount;
     public ToggleableCurrencyDisplay tvBalance;
+    public ImageView expandIcon;
 
     public GroupTitleViewHolder(View itemView) {
         super(itemView);
         tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
         tvBalance = (ToggleableCurrencyDisplay) itemView.findViewById(R.id.tvBalance);
+        tvAccountsCount = (TextView) itemView.findViewById(R.id.tvAccountsCount);
+        expandIcon = (ImageView) itemView.findViewById(R.id.expand);
     }
 }
