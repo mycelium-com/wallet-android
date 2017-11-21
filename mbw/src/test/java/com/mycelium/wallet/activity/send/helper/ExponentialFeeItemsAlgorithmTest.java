6fccd6faecc2e21389d7624bfcf8a07c5ae9a180
Leo Wandersleb <leo@leowandersleb.de>
2017-11-21 00:36:37 -0300

    ExponentialFeeItemsAlgorithm
    added ExponentialFeeItemsAlgorithm with documentation and unit tests
    also added a "unit test" that compares the algorithms a bit using printout. It's not meant to stay in the project in this form but it is helpful to get a feel for the tools.

---
+++ mbw/src/test/java/com/mycelium/wallet/activity/send/helper/ExponentialFeeItemsAlgorithmTest.java
@@ -0,0 +1,38 @@
+package com.mycelium.wallet.activity.send.helper;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExponentialFeeItemsAlgorithmTest {
+    private FeeItemsAlgorithm algorithm;
+    private final int MIN_POSITION = 12;
+    private final int MAX_POSITION = 22;
+    private final long MIN_VALUE = 1000;
+    private final long MAX_VALUE = 640000;
+
+    @Before
+    public void setup() {
+        algorithm = new ExponentialFeeItemsAlgorithm(MIN_VALUE, MIN_POSITION, MAX_VALUE, MAX_POSITION);
+    }
+
+    @Test
+    public void computeValue() throws Exception {
+        // due to integer values the following tests fail if the fee gets too low but it's sat/kB.
+        assertEquals(MIN_VALUE, algorithm.computeValue(MIN_POSITION));
+        assertEquals(MAX_VALUE, algorithm.computeValue(MAX_POSITION));
+        assertTrue(algorithm.computeValue(MIN_POSITION + 1) > MIN_VALUE);
+        assertTrue(algorithm.computeValue(MAX_POSITION - 1) < MAX_VALUE);
+    }
+
+    @Test
+    public void getMinPosition() throws Exception {
+        assertEquals(MIN_POSITION, algorithm.getMinPosition());
+    }
+
+    @Test
+    public void getMaxPosition() throws Exception {
+        assertEquals(MAX_POSITION, algorithm.getMaxPosition());
+    }
+}
---
+++ mbw/src/test/java/com/mycelium/wallet/activity/send/helper/FeeItemsAlgorithmTest.java
@@ -0,0 +1,31 @@
+package com.mycelium.wallet.activity.send.helper;
+
+import org.junit.Test;
+
+public class FeeItemsAlgorithmTest {
+    @Test
+    public void printThemAll() throws Exception {
+        long MAX_FEE = 640000;
+        long MIN_FEE = 3000;
+        int MIN_POSITION = 1;
+        int MAX_POSITION = 38; // 38 steps as that is about the count of steps the user would currently get over all fees
+
+        System.out.println("Linear");
+        // apparently the linear case ends up slightly off at the end
+        printStuff(new LinearAlgorithm(MIN_FEE, MIN_POSITION, MAX_FEE, MAX_POSITION));
+
+        System.out.println("Cubic");
+        // gets going very slowly
+        printStuff(new CubicAlgorithm(MIN_FEE, MIN_POSITION, MAX_FEE, MAX_POSITION));
+
+        System.out.println("Exponential");
+        // x^38 is off by one when using double :(
+        printStuff(new ExponentialFeeItemsAlgorithm(MIN_FEE, MIN_POSITION, MAX_FEE, MAX_POSITION));
+    }
+
+    private void printStuff(FeeItemsAlgorithm algorithm) {
+        for(int position=algorithm.getMinPosition(); position<=algorithm.getMaxPosition(); position++) {
+            System.out.println(algorithm.computeValue(position));
+        }
+    }
+}
