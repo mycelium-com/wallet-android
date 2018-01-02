acf44d7e781f7a21ca7e1b2c8f775babfaec8a60
Leo Wandersleb <leo@leowandersleb.de>
2018-01-01 22:18:19 -0300

    refactor: reduced code duplications in AccountManager
    Also removed some (repetitive) explicit types that Kotlin allows to be implicit.

--- mbw/src/main/java/com/mycelium/wallet/AccountManager.kt
+++ mbw/src/main/java/com/mycelium/wallet/AccountManager.kt
@@ -1,19 +1,18 @@
 package com.mycelium.wallet
 
 import com.google.common.collect.ImmutableMap
-import com.mycelium.wallet.AccountManager.putAll
-import com.mycelium.wallet.coinapult.CoinapultManager
-import com.mycelium.wallet.colu.ColuManager
 import com.mycelium.wallet.event.AccountChanged
 import com.mycelium.wapi.wallet.AccountProvider
 import com.mycelium.wapi.wallet.WalletAccount
+import com.mycelium.wapi.wallet.WalletAccount.Type
+import com.mycelium.wapi.wallet.WalletAccount.Type.*
 import com.mycelium.wapi.wallet.WalletManager
 import com.squareup.otto.Subscribe
 import java.util.*
-import kotlin.collections.HashMap
 
 object AccountManager : AccountProvider {
-    val accounts: HashMap<UUID, WalletAccount>  = hashMapOf()
+    val accounts: HashMap<UUID, WalletAccount> = hashMapOf()
+
     init {
         MbwManager.getInstance(WalletApplication.getInstance()).eventBus.register(this);
         fillAccounts()
@@ -28,60 +27,35 @@ object AccountManager : AccountProvider {
         accounts.putAll(walletManager.activeAccounts)
     }
 
-    override fun getAccounts(): ImmutableMap<UUID, WalletAccount> =
-            ImmutableMap.copyOf<UUID, WalletAccount>(accounts)
+    override fun getAccounts(): ImmutableMap<UUID, WalletAccount> = ImmutableMap.copyOf<UUID, WalletAccount>(accounts)
 
-    fun getBTCSingleAddressAccounts(): ImmutableMap<UUID, WalletAccount> {
-        return ImmutableMap.copyOf<UUID, WalletAccount>(accounts.filter {
-            it.value.type == WalletAccount.Type.BTCSINGLEADDRESS
-                    && !Utils.checkIsLinked(it.value, accounts.values)
-        })
-    }
+    fun getBTCSingleAddressAccounts(): ImmutableMap<UUID, WalletAccount> = ImmutableMap.copyOf<UUID, WalletAccount>(accounts.filter {
+        it.value.type == BTCSINGLEADDRESS && !Utils.checkIsLinked(it.value, accounts.values)
+    })
 
-    fun getBTCBip44Accounts(): ImmutableMap<UUID, WalletAccount> {
-        return ImmutableMap.copyOf<UUID, WalletAccount>(accounts.filter {
-            it.value.type == WalletAccount.Type.BTCBIP44
-        })
-    }
+    fun getBTCBip44Accounts() = getAccountsByType(BTCBIP44)
 
-    fun getBCHSingleAddressAccounts(): ImmutableMap<UUID, WalletAccount> {
-        return ImmutableMap.copyOf<UUID, WalletAccount>(accounts.filter {
-            it.value.type == WalletAccount.Type.BCHSINGLEADDRESS
-        })
-    }
+    fun getBCHSingleAddressAccounts() = getAccountsByType(BCHSINGLEADDRESS)
 
-    fun getBCHBip44Accounts(): ImmutableMap<UUID, WalletAccount> {
-        return ImmutableMap.copyOf<UUID, WalletAccount>(accounts.filter {
-            it.value.type == WalletAccount.Type.BCHBIP44
-        })
-    }
+    fun getBCHBip44Accounts() = getAccountsByType(BCHBIP44)
 
-    fun getCoinapultAccounts(): ImmutableMap<UUID, WalletAccount> {
-        return ImmutableMap.copyOf<UUID, WalletAccount>(accounts.filter {
-            it.value.type == WalletAccount.Type.COINAPULT
-        })
-    }
+    fun getCoinapultAccounts() = getAccountsByType(COINAPULT)
 
-    fun getColuAccounts(): ImmutableMap<UUID, WalletAccount> {
-        return ImmutableMap.copyOf<UUID, WalletAccount>(accounts.filter {
-            it.value.type == WalletAccount.Type.COLU
-        })
-    }
+    fun getColuAccounts() = getAccountsByType(COLU)
 
-    fun getDashAccounts(): ImmutableMap<UUID, WalletAccount> {
-        return ImmutableMap.copyOf<UUID, WalletAccount>(accounts.filter {
-            it.value.type == WalletAccount.Type.DASH
-        })
-    }
+    fun getDashAccounts() = getAccountsByType(DASH)
+
+    private fun getAccountsByType(coinType: Type) = ImmutableMap.copyOf<UUID, WalletAccount>(accounts.filter {
+        it.value.type == coinType
+    })
 
     override fun getAccount(uuid: UUID?): WalletAccount? = accounts.get(uuid)
 
     override fun hasAccount(uuid: UUID?): Boolean = accounts.containsKey(uuid)
 
-
     private fun HashMap<UUID, WalletAccount>.putAll(from: List<WalletAccount>) {
-        val result : MutableMap<UUID, WalletAccount> = mutableMapOf()
-        for(walletAccount in from) {
+        val result: MutableMap<UUID, WalletAccount> = mutableMapOf()
+        for (walletAccount in from) {
             result.put(walletAccount.id, walletAccount)
         }
         this.putAll(result)
