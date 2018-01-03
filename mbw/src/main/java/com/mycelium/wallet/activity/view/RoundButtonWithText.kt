a596b98fa800c70f9b94189a1fdadc420f90881d
Leo Wandersleb <leo@leowandersleb.de>
2018-01-03 19:33:31 -0300

    kotlin makes findViewById obsolete. had to try it out

--- mbw/src/main/java/com/mycelium/wallet/activity/view/RoundButtonWithText.kt
+++ mbw/src/main/java/com/mycelium/wallet/activity/view/RoundButtonWithText.kt
@@ -4,19 +4,16 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
-import android.widget.TextView
 import com.mycelium.wallet.R
+import kotlinx.android.synthetic.main.button_big_round_with_text.view.*
 
 
 class RoundButtonWithText(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
-
     init {
         View.inflate(context, R.layout.button_big_round_with_text, this)
 
         val a = context.theme.obtainStyledAttributes(attrs, R.styleable.RoundButtonWithText, 0, 0)
         try {
-            val roundButton = findViewById(R.id.roundButton) as RoundButton
-
             val circleColor = a.getColor(R.styleable.RoundButtonWithText_backgroundColor
                     , resources.getColor(R.color.black))
             roundButton.circleColor = circleColor
@@ -25,7 +22,6 @@ class RoundButtonWithText(context: Context, attrs: AttributeSet) : LinearLayout(
             roundButton.setImageDrawable(icon)
 
             val text = a.getString(R.styleable.RoundButtonWithText_text)
-            val textView = findViewById(R.id.textView) as TextView
             textView.text = text
         } finally {
             a.recycle()
@@ -33,7 +29,6 @@ class RoundButtonWithText(context: Context, attrs: AttributeSet) : LinearLayout(
     }
 
     override fun setOnClickListener(l: OnClickListener?) {
-        val roundButton = findViewById(R.id.roundButton) as RoundButton
         roundButton.setOnClickListener(l)
     }
 }
