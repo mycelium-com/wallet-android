e27a20846d97bd117a427e7be512cd88f112acb2
Leo Wandersleb <leo@leowandersleb.de>
2018-01-26 13:48:06 -0300

    refactor: made unit test more readable

--- wapi/src/test/java/com/mycelium/wapi/ColuTransferInstructionsParserTest.java
+++ wapi/src/test/java/com/mycelium/wapi/ColuTransferInstructionsParserTest.java
@@ -1,5 +1,7 @@
 package com.mycelium.wapi;
 
+import com.subgraph.orchid.encoders.Hex;
+
 import org.junit.Test;
 
 import java.util.List;
@@ -16,21 +18,11 @@ public class ColuTransferInstructionsParserTest {
 
     @Test
     public void retrieveOutputIndexesFromScript() throws Exception {
-        /*
-            { skip: false, range: true, percent: false, output: 1, amount: 7000 } - 0x40 0x01 0x20 0x73
-            { skip: false, range: false, percent: true, output: 5, amount: 10}    - 0x25 0x0a
-            { skip: false, range: false, percent: false, output: 0, amount: 3}    - 0x00 0x03
-
-            { skip: false, range: false, percent: false, output: 0, amount: 3}    - 0x00 0x03
-            { skip: false, range: false, percent: false, output: 0, amount: 3}    - 0x00 0x03
-            { skip: false, range: false, percent: false, output: 0, amount: 3}    - 0x00 0x03
-            { skip: false, range: false, percent: false, output: 0, amount: 3}    - 0x00 0x03
-            { skip: false, range: false, percent: false, output: 0, amount: 3}    - 0x00 0x03
-            { skip: false, range: false, percent: false, output: 0, amount: 3}    - 0x00 0x03
-            { skip: false, range: false, percent: false, output: 0, amount: 3}    - 0x00 0x03
-         */
-
-        byte[] script = {0x00, 0x00, 0x43, 0x43, 0x02, 0x15, 0x40, 0x01, 0x20, 0x73, 0x25, 0x0a, 0x00, 0x03};
+        byte[] script = Hex.decode("00 00 43 43" + // colored coin protocol
+                "02 15 " +      // TODO: document
+                "40 01 20 73" + // output: 1, amount: 7000
+                "25 0a" +       // output: 5, amount: 10
+                "00 03");       // output: 0, amount: 3
         List<Integer> outputIndexes = ColuTransferInstructionsParser.retrieveOutputIndexesFromScript(script);
         assertEquals(3, outputIndexes.size());
         assertEquals(1, (int)outputIndexes.get(0));
@@ -40,8 +32,7 @@ public class ColuTransferInstructionsParserTest {
 
     @Test
     public void retrieveOutputIndexesFromTooSmallScript() throws Exception {
-
-        byte[] script = {0x00, 0x00, 0x42, 0x40, 0x02};
+        byte[] script = Hex.decode("00 00 43 43 02");
         List<Integer> outputIndexes = ColuTransferInstructionsParser.retrieveOutputIndexesFromScript(script);
         assertEquals(0, outputIndexes.size());
     }
