89a77f0e17798b55801ed74f26a703c6b35fd951
Nadia Poletova <poletova-n@mail.ru>
2018-06-06 19:38:16 +0300

    #958 correction methods in Bip44Account

--- wapi/src/main/java/com/mycelium/wapi/wallet/bip44/Bip44Account.java
+++ wapi/src/main/java/com/mycelium/wapi/wallet/bip44/Bip44Account.java
@@ -107,10 +107,7 @@ public class Bip44Account extends AbstractAccount implements ExportableAccount {
         if (_context.isArchived()) {
             return;
         }
-        _context.setArchived(true);
         clearInternalStateInt(true);
-        _context.persistIfNecessary(_backing);
-
     }
 
     @Override
@@ -118,9 +115,7 @@ public class Bip44Account extends AbstractAccount implements ExportableAccount {
         if (!_context.isArchived()) {
             return;
         }
-        _context.setArchived(false);
         clearInternalStateInt(false);
-        _context.persistIfNecessary(_backing);
     }
 
     @Override
