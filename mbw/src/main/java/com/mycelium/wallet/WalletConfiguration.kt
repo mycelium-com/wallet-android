0aa56a229b7c95ada6e1fb5ceaeb50d21f191fa8
itserg <itserg2008@gmail.com>
2018-06-12 16:50:29 +0300

    Updates according to latest nodes.json changes

--- mbw/src/main/java/com/mycelium/wallet/WalletConfiguration.kt
+++ mbw/src/main/java/com/mycelium/wallet/WalletConfiguration.kt
@@ -13,6 +13,7 @@ import retrofit2.http.GET
 import java.io.IOError
 import java.io.IOException
 import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
 
 interface  MyceliumNodesApi {
@@ -20,12 +21,20 @@ interface  MyceliumNodesApi {
     fun getNodes(): Call<MyceliumNodesResponse>
 }
 
+// A set of classes for parsing nodes.json file
+
+// MyceliumNodesResponse is intended for parsing nodes.json file
 class MyceliumNodesResponse(@SerializedName("BTC-testnet") val btcTestnet: BTCNetResponse,
                             @SerializedName("BTC-mainnet") val btcMainnet: BTCNetResponse)
 
-class BTCNetResponse(val electrumX: ElectrumXResponse)
+// BTCNetResponse is intended for parsing nodes.json file
+class BTCNetResponse(val electrumx: ElectrumXResponse)
 
-class ElectrumXResponse(val nodes : Array<String>)
+// ElectrumXResponse is intended for parsing nodes.json file
+class ElectrumXResponse(val primary : Array<ElectrumServerResponse>, backup: Array<ElectrumServerResponse>)
+
+// ElectrumServerResponse is intended for parsing nodes.json file
+class ElectrumServerResponse(val url: String)
 
 class WalletConfiguration(private val prefs: SharedPreferences, val network : NetworkParameters) {
 
@@ -33,13 +42,14 @@ class WalletConfiguration(private val prefs: SharedPreferences, val network : Ne
         updateConfig()
     }
 
+    // Makes a request to S3 storage to retrieve nodes.json and parses it to extract electrum servers list
     fun updateConfig() {
         var latch = CountDownLatch(1)
 
         thread(start = true) {
             try {
                 val resp = Retrofit.Builder()
-                        .baseUrl("https://mycelium-wallet.s3.amazonaws.com")
+                        .baseUrl(AMAZON_S3_STORAGE_ADDRESS)
                         .addConverterFactory(GsonConverterFactory.create())
                         .build()
                         .create(MyceliumNodesApi::class.java)
@@ -49,13 +59,10 @@ class WalletConfiguration(private val prefs: SharedPreferences, val network : Ne
                 if (resp.isSuccessful) {
                     val myceliumNodesResponse = resp.body()
 
-                    val nodes = mutableSetOf(*if (network.isTestnet) {
-                        myceliumNodesResponse?.btcTestnet?.electrumX?.nodes!!
-                    } else {
-                        myceliumNodesResponse?.btcMainnet?.electrumX?.nodes!!
-                    })
-
-                        // the new peers are different from the old peers and not empty. store them!
+                    val nodes = if (network.isTestnet())
+                        myceliumNodesResponse?.btcTestnet?.electrumx?.primary?.map { it.url }?.toSet()
+                    else
+                        myceliumNodesResponse?.btcMainnet?.electrumx?.primary?.map { it.url }?.toSet()
                     prefs.edit().putStringSet(PREFS_ELECTRUM_SERVERS, nodes).apply()
                 }
 
@@ -65,23 +72,33 @@ class WalletConfiguration(private val prefs: SharedPreferences, val network : Ne
             }
         }
 
-        latch.await()
+        // Wait for S3 server response for MAX_WAITING_TIMEOUT milliseconds
+        try {
+            latch.await(MAX_WAITING_TIMEOUT, TimeUnit.MILLISECONDS)
+        } catch (ex : InterruptedException) {
+        }
     }
 
+    // Returns the set of electrum servers
     val electrumServers: Set<String>
         get() = prefs.getStringSet(PREFS_ELECTRUM_SERVERS, mutableSetOf(*BuildConfig.ElectrumServers))
 
+    // Returns the list of TcpEndpoint objects
     fun getElectrumEndpoints(): List<TcpEndpoint>
     {
         val result = ArrayList<TcpEndpoint>()
         electrumServers.forEach {
-            val strs = it.split(":")
+            val strs = it.replace(TCP_TLS_PREFIX, "").split(":")
             result.add(TcpEndpoint(strs[0], strs[1].toInt()))
         }
         return result
     }
 
     companion object {
+        const val MAX_WAITING_TIMEOUT = 10000L
         const val PREFS_ELECTRUM_SERVERS = "electrum_servers"
+
+        const val TCP_TLS_PREFIX = "tcp-tls://"
+        const val AMAZON_S3_STORAGE_ADDRESS = "https://mycelium-wallet.s3.amazonaws.com"
     }
 }
