41eb306197d039c5214d57ee2085771993c81e38
SergeyLappo <sergeylappo@gmail.com>
2018-08-11 14:26:35 +0300

    Fixed totally wrong accounts list behavior

--- mbw/src/main/java/com/mycelium/wallet/activity/modern/model/accounts/TotalViewModel.kt
+++ mbw/src/main/java/com/mycelium/wallet/activity/modern/model/accounts/TotalViewModel.kt
@@ -7,4 +7,19 @@ import com.mycelium.wapi.wallet.currency.CurrencySum
  */
 class TotalViewModel(val balance: CurrencySum) : AccountListItem {
     override fun getType() = AccountListItem.Type.TOTAL_BALANCE_TYPE
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TotalViewModel
+
+        if (balance != other.balance) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return balance.hashCode()
+    }
 }
