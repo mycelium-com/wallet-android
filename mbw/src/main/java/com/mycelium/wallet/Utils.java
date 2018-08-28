60a4fe4794dc3dd58b58f60623529f1e543b18b8
Nadia Poletova <poletova-n@mail.ru>
2018-08-28 13:58:47 +0300

    #1237 update import

--- mbw/src/main/java/com/mycelium/wallet/Utils.java
+++ mbw/src/main/java/com/mycelium/wallet/Utils.java
@@ -54,7 +54,7 @@ import android.support.annotation.StringRes;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
-import android.text.ClipboardManager;
+import android.content.ClipboardManager;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.util.SparseArray;
