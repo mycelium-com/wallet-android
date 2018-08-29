30e111bf0a3ef6c678ea78fa59d6853b4f314dbc
itserg <itserg2008@gmail.com>
2018-08-29 15:02:03 +0300

    Added pendingChange to Balance

--- walletcore/src/main/java/com/mycelium/wapi/wallet/coins/Balance.java
+++ walletcore/src/main/java/com/mycelium/wapi/wallet/coins/Balance.java
@@ -18,9 +18,15 @@ public class Balance {
      */
     public final Value pendingSending;
 
-    public Balance(Value confirmed, Value pendingReceiving, Value pendingSending) {
+    /**
+     * The sum of the outputs being sent from the address set to itself
+     */
+    public final Value pendingChange;
+
+    public Balance(Value confirmed, Value pendingReceiving, Value pendingSending, Value pendingChange) {
         this.confirmed = confirmed;
         this.pendingReceiving = pendingReceiving;
         this.pendingSending = pendingSending;
+        this.pendingChange = pendingChange;
     }
 }
