8054939bc0ca61106d73921c078d1cf9b3b28034
Roman <iamromanzuev@gmail.com>
2018-09-06 13:54:16 +0300

    full rename

--- mbw/src/main/java/com/mycelium/wallet/MbwManager.java
+++ mbw/src/main/java/com/mycelium/wallet/MbwManager.java
@@ -103,11 +103,10 @@ import com.mycelium.wallet.extsig.trezor.TrezorManager;
 import com.mycelium.wallet.lt.LocalTraderManager;
 import com.mycelium.wallet.modularisation.GooglePlayModuleCollection;
 import com.mycelium.wallet.modularisation.SpvBchFetcher;
-import com.mycelium.wallet.modularisation.TSMHelper;
+import com.mycelium.wallet.modularisation.MEBHelper;
 import com.mycelium.wallet.persistence.MetadataStorage;
 import com.mycelium.wallet.persistence.TradeSessionDb;
 import com.mycelium.wallet.wapi.SqliteWalletManagerBackingWrapper;
-import com.mycelium.wapi.api.WapiClient;
 import com.mycelium.wapi.api.WapiClientElectrumX;
 import com.mycelium.wapi.api.jsonrpc.TcpEndpoint;
 import com.mycelium.wapi.wallet.AccountProvider;
@@ -193,7 +192,7 @@ public class MbwManager {
     private final ExternalSignatureDeviceManager _trezorManager;
     private final KeepKeyManager _keepkeyManager;
     private final LedgerManager _ledgerManager;
-    private final TSMHelper _tsmHelper;
+    private final MEBHelper _mebHelper;
     private final WapiClientElectrumX _wapi;
 
     private final LtApiClient _ltApi;
@@ -313,7 +312,7 @@ public class MbwManager {
         _keepkeyManager = new KeepKeyManager(_applicationContext, getNetwork(), getEventBus());
         _ledgerManager = new LedgerManager(_applicationContext, getNetwork(), getEventBus());
         _walletManager = createWalletManager(_applicationContext, _environment);
-        _tsmHelper = new TSMHelper(_applicationContext);
+        _mebHelper = new MEBHelper(_applicationContext);
 
         _eventTranslator = new EventTranslator(new Handler(), _eventBus);
         _exchangeRateManager.subscribe(_eventTranslator);
@@ -1298,8 +1297,8 @@ public class MbwManager {
         return _ledgerManager;
     }
 
-    public TSMHelper getTSMHelper() {
-        return _tsmHelper;
+    public MEBHelper getMEBHelper() {
+        return _mebHelper;
     }
 
     public WapiClientElectrumX getWapi() {
