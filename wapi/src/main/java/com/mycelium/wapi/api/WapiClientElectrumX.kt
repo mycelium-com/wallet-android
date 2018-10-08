6c0c963183af6b496310b834bdf695d93cf3583b
SergeyLappo <sergeylappo@gmail.com>
2018-10-08 12:47:48 +0300

    Fixed crash if tx fee estimation fails

--- wapi/src/main/java/com/mycelium/wapi/api/WapiClientElectrumX.kt
+++ wapi/src/main/java/com/mycelium/wapi/api/WapiClientElectrumX.kt
@@ -268,6 +268,10 @@ class WapiClientElectrumX(
             val feeEstimationMap = FeeEstimationMap()
 
             estimatesArray.forEachIndexed { index, response ->
+                // This might happened if server haven't got enough info.
+                if (response.getResult(Double::class.java)!! == -1.0) {
+                    return WapiResponse<MinerFeeEstimationResponse>(Wapi.ERROR_CODE_INTERNAL_SERVER_ERROR, null)
+                }
                 feeEstimationMap[blocks[index]] = Bitcoins.valueOf(response.getResult(Double::class.java)!!)
             }
             return WapiResponse(MinerFeeEstimationResponse(FeeEstimation(feeEstimationMap, Date())))
