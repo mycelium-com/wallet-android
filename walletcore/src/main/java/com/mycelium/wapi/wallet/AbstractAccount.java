762b75b091672e3d48f2cda213365324908b73cf
Leo Wandersleb <leo@leowandersleb.de>
2018-12-05 01:02:29 -0300

    fixed a logic error

--- walletcore/src/main/java/com/mycelium/wapi/wallet/AbstractAccount.java
+++ walletcore/src/main/java/com/mycelium/wapi/wallet/AbstractAccount.java
@@ -1431,9 +1431,11 @@ public abstract class AbstractAccount extends SynchronizeAbleWalletAccount {
       @Override
       public PublicKey findPublicKeyByAddress(Address address) {
          PublicKey publicKey = getPublicKeyForAddress(address);
-         if (address.getType() == AddressType.P2SH_P2WPKH || address.getType() == AddressType.P2WPKH && publicKey != null) {
-            return new PublicKey(publicKey.getPubKeyCompressed());
-         } else if (publicKey != null) {
+         if (publicKey != null) {
+            if (address.getType() == AddressType.P2SH_P2WPKH
+                    || address.getType() == AddressType.P2WPKH) {
+               return new PublicKey(publicKey.getPubKeyCompressed());
+            }
             return publicKey;
          }
          // something unexpected happened - the account might be in a undefined state
