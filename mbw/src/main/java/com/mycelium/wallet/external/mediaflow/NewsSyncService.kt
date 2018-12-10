e5a4cf0e41358d7898d9335f3ec3b0806cde5495
elvis <xElvis89x@gmail.com>
2018-12-10 23:38:08 +0300

    update from develop

--- mbw/src/main/java/com/mycelium/wallet/external/mediaflow/NewsSyncService.kt
+++ mbw/src/main/java/com/mycelium/wallet/external/mediaflow/NewsSyncService.kt
@@ -36,7 +36,7 @@ class NewsSyncService : Service() {
         super.onStart(intent, startId)
         val preference = getSharedPreferences(NewsConstants.NEWS_PREF, Context.MODE_PRIVATE)!!
         val lastUpdateTime = preference.getString(NewsConstants.UPDATE_TIME, null);
-        NewsUpdate(MbwManager.getInstance(this).eventBus, lastUpdateTime, {
+        NewsUpdate(MbwManager.getEventBus(), lastUpdateTime, {
             val formattedDate = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.UK).format(Date())
             preference.edit()
                     .putString(NewsConstants.UPDATE_TIME, formattedDate)
