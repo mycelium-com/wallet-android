c2ec1506e7f757c08bda00f3c817adf24f03eb03
Leo Wandersleb <leo@leowandersleb.de>
2018-12-19 00:59:53 -0300

    fixed NPE with SimplexErrors
    SimplexError was instantiated without the needed properties in several
    places. The OttoBus subscriber thus ran into NPEs.

--- mbw/src/main/java/com/mycelium/wallet/simplex/SimplexServer.java
+++ mbw/src/main/java/com/mycelium/wallet/simplex/SimplexServer.java
@@ -3,9 +3,7 @@ package com.mycelium.wallet.simplex;
 
 import android.content.Context;
 import android.os.Handler;
-import android.util.Log;
 
-import com.google.common.base.Charsets;
 import com.mycelium.wallet.R;
 import com.mycelium.wallet.Utils;
 import com.squareup.okhttp.Callback;
@@ -18,63 +16,54 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
-/**
- * Created by tomb on 11/20/16.
- */
-
-public class SimplexServer {
-
+class SimplexServer {
     private final String baseUrl = "https://mycelium.simplex-affiliates.com";
+    private OkHttpClient client = new OkHttpClient();
 
-   private OkHttpClient client = new OkHttpClient();
+    String getAuthRequestUrl() {
+        return baseUrl + "/val";
+    }
 
-   public String getAuthRequestUrl() {
-      return baseUrl + "/val";
-   }
+    void getNonceAsync(final Bus eventBus, final Context context) {
+        Request request = new Request.Builder()
+                .url(baseUrl + "/android")
+                .build();
 
-   public void getNonceAsync(final Bus eventBus, final Context context) {
-      Request request = new Request.Builder()
-              .url(baseUrl + "/android")
-              .build();
-
-      client.newCall(request).enqueue(new Callback() {
-         @Override
-         public void onFailure(Request request, IOException e) {
-            Log.w("simplex", "get nonce Failed!");
-            SimplexError error = new SimplexError();
-            error.activityHandler = new Handler(context.getMainLooper());
-            if (!Utils.isConnected(context)) {
-               error.message = context.getResources().getString(R.string.no_network_connection);
-            } else {
-               error.message = e.getMessage();
-            }
-            eventBus.post(error);
-         }
-
-         @Override
-         public void onResponse(com.squareup.okhttp.Response response) throws IOException {
-            if (!response.isSuccessful()) {
-               //change the UI to retry screen
-               Log.w("simplex", "get nonce not successful.");
-               SimplexError error = new SimplexError();
-               eventBus.post(error);
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Handler handler = new Handler(context.getMainLooper());
+                String message;
+                if (!Utils.isConnected(context)) {
+                    message = context.getResources().getString(R.string.no_network_connection);
+                } else {
+                    message = e.getMessage();
+                }
+                eventBus.post(new SimplexError(handler, message));
             }
 
-            Log.d("simplex","start parse nonce response...");
-            String jsonData = response.body().string();
-            try {
-                JSONObject Jobject = new JSONObject(jsonData);
-                SimplexNonceResponse nonceResponse = new SimplexNonceResponse();
-                nonceResponse.simplexNonce = Jobject.getString("nonce");
-                nonceResponse.googleNonce =  Jobject.getLong("google");
-                eventBus.post(nonceResponse);
-            } catch (JSONException e) {
-                Log.w("simplex","failed to parse getNonce json!");
-                e.printStackTrace();
-                SimplexError error = new SimplexError();
-                eventBus.post(error);
+            @Override
+            public void onResponse(com.squareup.okhttp.Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    //change the UI to retry screen
+                    String message = "get nonce not successful.";
+                    SimplexError error = new SimplexError(new Handler(context.getMainLooper()), message);
+                    eventBus.post(error);
+                }
+
+                String jsonData = response.body().string();
+                try {
+                    JSONObject Jobject = new JSONObject(jsonData);
+                    SimplexNonceResponse nonceResponse = new SimplexNonceResponse();
+                    nonceResponse.simplexNonce = Jobject.getString("nonce");
+                    nonceResponse.googleNonce = Jobject.getLong("google");
+                    eventBus.post(nonceResponse);
+                } catch (JSONException e) {
+                    String message = "failed to parse getNonce json!";
+                    SimplexError error = new SimplexError(new Handler(context.getMainLooper()), message);
+                    eventBus.post(error);
+                }
             }
-         }
-      });
-   }
+        });
+    }
 }
