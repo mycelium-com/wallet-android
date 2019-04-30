3924c882a145ec973430d72ec26ffb2363792915
Stanislav Obrazcoff <obrazcoff@gmail.com>
2019-04-30 19:57:02 +0200

    Tuneup sync class

--- walletcore/src/main/java/com/mycelium/wapi/wallet/SynchronizeFinishedListener.kt
+++ walletcore/src/main/java/com/mycelium/wapi/wallet/SynchronizeFinishedListener.kt
@@ -4,26 +4,19 @@ import com.mycelium.wapi.wallet.manager.WalletListener
 import java.util.concurrent.CountDownLatch
 
 class SynchronizeFinishedListener: WalletListener{
-    var latch: CountDownLatch? = null
-
-    init {
-        latch = CountDownLatch(1)
-    }
+    var latch: CountDownLatch = CountDownLatch(1)
 
     override fun syncStarted() {
-        println("SynchronizeFinishedListener: Sync started")
     }
 
     override fun syncStopped() {
-        latch?.countDown()
+        latch.countDown()
     }
 
     fun waitForSyncFinished() {
         try {
-            latch?.await()
-        } catch (exc: InterruptedException) {
-            System.out.println("WalletConsole: Sync account exception");
-        }
+            latch.await()
+        } catch (exc: InterruptedException) {}
     }
 
 }
