2982839791ba9a10f542a28416d2e34cc11d57b0
elvis <xElvis89x@gmail.com>
2019-05-17 13:03:50 +0300

    added sofello to credit card buy option

--- mbw/src/main/java/com/mycelium/wallet/external/SimplexServiceDescription.java
+++ mbw/src/main/java/com/mycelium/wallet/external/SimplexServiceDescription.java
@@ -1,7 +1,10 @@
 package com.mycelium.wallet.external;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 import com.google.common.base.Optional;
@@ -12,6 +15,9 @@ import com.mycelium.wallet.simplex.SimplexMainActivity;
 
 
 public class SimplexServiceDescription extends BuySellServiceDescriptor {
+
+   public static final String SOFELLO = "https://app.s4f3.io/sdk/quickbuy.html?appId=1234-5678&lang=en&country=no";
+
    public SimplexServiceDescription() {
       super(R.string.si_buy_sell, R.string.si_buy_sell_description, R.string.si_setting_show_button_summary, R.drawable.credit_card_buy);
    }
@@ -24,11 +30,25 @@ public class SimplexServiceDescription extends BuySellServiceDescriptor {
       }
       Optional<Address> receivingAddress = mbwManager.getSelectedAccount().getReceivingAddress();
       if (receivingAddress.isPresent()) {
-         Address address = receivingAddress.get();
-         Intent intent = new Intent(context, SimplexMainActivity.class);
-         intent.putExtra("walletAddress",address.toString());
-         context.startActivity(intent);
-
+         final Address address = receivingAddress.get();
+         String[] regions = {context.getString(R.string.europe), context.getString(R.string.asia),
+                 context.getString(R.string.united_states), context.getString(R.string.australia)};
+         new AlertDialog.Builder(context, R.style.BuySell_Dialog)
+                 .setTitle(R.string.select_you_region)
+                 .setItems(regions, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                       Intent intent;
+                       if (i == 0) {
+                          intent = new Intent(Intent.ACTION_VIEW, Uri.parse(SOFELLO));
+                       } else {
+                          intent = new Intent(context, SimplexMainActivity.class);
+                          intent.putExtra("walletAddress", address.toString());
+                       }
+                       context.startActivity(intent);
+                    }
+                 })
+                 .create().show();
       } else {
          Toast.makeText(context, "Simplex cannot start - no available address.", Toast.LENGTH_LONG).show();
       }
