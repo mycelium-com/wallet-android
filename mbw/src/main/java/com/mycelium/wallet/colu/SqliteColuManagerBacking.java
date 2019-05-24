d2f94aeea895fdbcb7832a0d06d4f1d21361c53c
itserg <itserg2008@gmail.com>
2019-05-24 14:06:26 +0300

    Fixed COLU db upgrade

--- mbw/src/main/java/com/mycelium/wallet/colu/SqliteColuManagerBacking.java
+++ mbw/src/main/java/com/mycelium/wallet/colu/SqliteColuManagerBacking.java
@@ -611,7 +611,7 @@ public class SqliteColuManagerBacking implements WalletBacking<ColuAccountContex
 
    private class OpenHelper extends SQLiteOpenHelper {
       private static final String DATABASE_NAME = "columanagerbacking.db";
-      private static final int DATABASE_VERSION = 7;
+      private static final int DATABASE_VERSION = 8;
       private Context context;
 
       OpenHelper(Context context) {
@@ -635,7 +635,10 @@ public class SqliteColuManagerBacking implements WalletBacking<ColuAccountContex
 
       @Override
       public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+         if (oldVersion < DATABASE_VERSION) {
+            db.execSQL("ALTER TABLE single ADD COLUMN coinId TEXT");
+            db.execSQL("ALTER TABLE single ADD COLUMN publicKey BLOB");
+         }
       }
 
       @Override
