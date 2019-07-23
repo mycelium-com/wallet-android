45f661f57bbb19087cf1a4fe8e50ea332aa05445
mikhailz <zisman.mikhail@gmail.com>
2019-07-23 20:59:46 +0300

    TEST: create new account

---
+++ mbw/src/androidTest/java/com/mycelium/wallet/activity/ElapsedTimeIdlingResource.java
@@ -0,0 +1,34 @@
+package com.mycelium.wallet.activity;
+
+import android.support.test.espresso.IdlingResource;
+
+public class ElapsedTimeIdlingResource implements IdlingResource {
+  private final long startTime;
+  private final long waitingTime;
+  private ResourceCallback resourceCallback;
+
+  public ElapsedTimeIdlingResource(long waitingTime) {
+    this.startTime = System.currentTimeMillis();
+    this.waitingTime = waitingTime;
+  }
+
+  @Override
+  public String getName() {
+    return ElapsedTimeIdlingResource.class.getName() + ":" + waitingTime;
+  }
+
+  @Override
+  public boolean isIdleNow() {
+    long elapsed = System.currentTimeMillis() - startTime;
+    boolean idle = (elapsed >= waitingTime);
+    if (idle) {
+      resourceCallback.onTransitionToIdle();
+    }
+    return idle;
+  }
+
+  @Override
+  public void registerIdleTransitionCallback(ResourceCallback resourceCallback) {
+    this.resourceCallback = resourceCallback;
+  }
+}
