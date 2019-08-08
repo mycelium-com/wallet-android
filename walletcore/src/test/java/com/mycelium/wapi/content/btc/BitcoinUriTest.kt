cc996723ca81071aa86486719508c5ab68961ecd
agneslovelace <hamman12386@gmail.com>
2019-08-08 15:09:04 +0300

    Fix BIP21 test vectors with %% encoding fail to scan

--- walletcore/src/test/java/com/mycelium/wapi/content/btc/BitcoinUriTest.kt
+++ walletcore/src/test/java/com/mycelium/wapi/content/btc/BitcoinUriTest.kt
@@ -43,16 +43,14 @@ class BitcoinUriTest {
 
     @Test
     fun testMainnetAddressLabelWithPercentageEncoding() {
-        // with only address
         testParse("bitcoin:1A3fouaDJA4RRLnQmFxQRh98gr8cFGvwdN?label=counterparty%3A%20burn%20address", PN,
                 BitcoinUri.from(AddressUtils.from(BitcoinMain.get(), "1A3fouaDJA4RRLnQmFxQRh98gr8cFGvwdN"), null, "counterparty: burn address", null))
     }
 
     @Test
     fun testMainnetBip21Request50BTCWithMessage() {
-        // with only address
-        testParse("bitcoin:175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W?amount=50&label=Luke-Jr&message=Donation%20for%20project%20xyz", PN,
-                BitcoinUri.from(AddressUtils.from(BitcoinMain.get(), "175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W"), null, "Luke-Jr", null))
+        testParse("bitcoin:1A3fouaDJA4RRLnQmFxQRh98gr8cFGvwdN?amount=50&label=Luke-Jr&message=Donation%20for%20project%20xyz", PN,
+                BitcoinUri.from(AddressUtils.from(BitcoinMain.get(), "1A3fouaDJA4RRLnQmFxQRh98gr8cFGvwdN"), Value.valueOf(BitcoinMain.get(), 5000000000), "Luke-Jr", null))
     }
 
     @Test
@@ -61,6 +59,14 @@ class BitcoinUriTest {
         testParse("bitcoin:mq7se9wy2egettFxPbmn99cK8v5AFq55Lx?amount=0.11", TN,
                 BitcoinUri.from(AddressUtils.from(BitcoinTest.get(), "mq7se9wy2egettFxPbmn99cK8v5AFq55Lx"),
                         Value.valueOf(BitcoinMain.get(), 11000000), null, null))
+    }
+
+    @Test
+    fun testTestnetAddressInvalidForm() {
+        // invalid form that starts with "bitcoin://"
+        testParse("bitcoin://mq7se9wy2egettFxPbmn99cK8v5AFq55Lx", TN,
+                BitcoinUri.from(AddressUtils.from(BitcoinTest.get(), "mq7se9wy2egettFxPbmn99cK8v5AFq55Lx"),
+                        null, null, null))
     }
 
     private fun testParse(url: String, np: NetworkParameters, expected: BitcoinUri) {
