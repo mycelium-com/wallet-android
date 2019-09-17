a0ba5a10254c6690db34633f05d3e6e90bdf450f
Leo Wandersleb <leo@leowandersleb.de>
2019-09-17 23:56:07 +1200

    cleanups

--- bitlib/src/main/java/com/mrd/bitlib/util/VarInt.java
+++ bitlib/src/main/java/com/mrd/bitlib/util/VarInt.java
@@ -121,22 +121,22 @@ public class VarInt {
 
     /** Parse 4 bytes from the byte array (starting at the offset) as unsigned 32-bit integer in little endian format. */
     public static long readUint32(byte[] bytes, int offset) {
-        return (bytes[offset] & 0xffl) |
-                ((bytes[offset + 1] & 0xffl) << 8) |
-                ((bytes[offset + 2] & 0xffl) << 16) |
-                ((bytes[offset + 3] & 0xffl) << 24);
+        return (bytes[offset] & 0xffL) |
+                ((bytes[offset + 1] & 0xffL) << 8) |
+                ((bytes[offset + 2] & 0xffL) << 16) |
+                ((bytes[offset + 3] & 0xffL) << 24);
     }
 
     /** Parse 8 bytes from the byte array (starting at the offset) as signed 64-bit integer in little endian format. */
     public static long readInt64(byte[] bytes, int offset) {
-        return (bytes[offset] & 0xffl) |
-                ((bytes[offset + 1] & 0xffl) << 8) |
-                ((bytes[offset + 2] & 0xffl) << 16) |
-                ((bytes[offset + 3] & 0xffl) << 24) |
-                ((bytes[offset + 4] & 0xffl) << 32) |
-                ((bytes[offset + 5] & 0xffl) << 40) |
-                ((bytes[offset + 6] & 0xffl) << 48) |
-                ((bytes[offset + 7] & 0xffl) << 56);
+        return (bytes[offset] & 0xffL) |
+                ((bytes[offset + 1] & 0xffL) << 8) |
+                ((bytes[offset + 2] & 0xffL) << 16) |
+                ((bytes[offset + 3] & 0xffL) << 24) |
+                ((bytes[offset + 4] & 0xffL) << 32) |
+                ((bytes[offset + 5] & 0xffL) << 40) |
+                ((bytes[offset + 6] & 0xffL) << 48) |
+                ((bytes[offset + 7] & 0xffL) << 56);
     }
 
     /** Write 2 bytes to the byte array (starting at the offset) as unsigned 16-bit integer in little endian format. */
