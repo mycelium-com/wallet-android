9531712020c3380e22ad905de96ca82dd7e7cc7d
agneslovelace <hamman12386@gmail.com>
2019-11-05 10:13:15 +0300

    Fix catch error when server unav.

--- walletcore/src/main/java/com/mycelium/wapi/wallet/eth/EtherscanIoFetcher.kt
+++ walletcore/src/main/java/com/mycelium/wapi/wallet/eth/EtherscanIoFetcher.kt
@@ -14,6 +14,8 @@ import kotlinx.coroutines.withContext
 import java.math.BigInteger
 import java.net.URL
 import java.util.concurrent.TimeUnit
+import java.util.logging.Level
+import java.util.logging.Logger
 
 object EtherscanIoFetcher {
 
@@ -22,32 +24,36 @@ object EtherscanIoFetcher {
         GlobalScope.launch {
             val apiKey = "KWQPBBFJQYAT5P447MM8322R5BVY8C2MG2"
             val subDomain = if (coinType == EthMain) PRODNET_SUBDOMAIN else TESTNET_SUBDOMAIN
-            val urlString = "http://${subDomain}.etherscan.io/api?module=account&action=txlist&address=0xc1F1583b65C4838418B48039cf5643e119c48A02&startblock=0&endblock=99999999&sort=asc&apikey=${apiKey}"
+            val urlString = "http://${subDomain}.etherscan.io/api?module=account&action=txlist&address=${receivingAddress}&startblock=0&endblock=99999999&sort=asc&apikey=${apiKey}"
             val mapper = ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
             withContext(Dispatchers.IO) {
-                val remoteTxs = mapper.readValue(URL(urlString), EtherscanApiTxlist::class.java).result
-                val localTxs = backing.getTransactionSummaries(0, Long.MAX_VALUE, receivingAddress)
-                val remoteIds = remoteTxs.map { it.hash }
-                val localIds = localTxs.map { "0x" + HexUtils.toHex(it.id) }
-                val toAdd = remoteTxs.filter { !localIds.contains(it.hash) }
-                val toUpdate = remoteTxs.filter { it.hash in remoteIds.intersect(localIds) }
-                val toDelete = localTxs.filter {
-                    !remoteIds.contains("0x" + HexUtils.toHex(it.id)) and
-                            (System.currentTimeMillis() / 1000 - it.timestamp > TIME_WAIT_FOR_CONFIRMATION)
-                }.map { "0x" + HexUtils.toHex(it.id) }
+                try {
+                    val remoteTxs = mapper.readValue(URL(urlString), EtherscanApiTxlist::class.java).result
+                    val localTxs = backing.getTransactionSummaries(0, Long.MAX_VALUE, receivingAddress)
+                    val remoteIds = remoteTxs.map { it.hash }
+                    val localIds = localTxs.map { "0x" + HexUtils.toHex(it.id) }
+                    val toAdd = remoteTxs.filter { !localIds.contains(it.hash) }
+                    val toUpdate = remoteTxs.filter { it.hash in remoteIds.intersect(localIds) }
+                    val toDelete = localTxs.filter {
+                        !remoteIds.contains("0x" + HexUtils.toHex(it.id)) and
+                                (System.currentTimeMillis() / 1000 - it.timestamp > TIME_WAIT_FOR_CONFIRMATION)
+                    }.map { "0x" + HexUtils.toHex(it.id) }
 
-                toAdd.forEach { tx ->
-                    backing.putTransaction(tx.blockNumber, tx.timeStamp, tx.hash,
-                            "", tx.from, tx.to, Value.valueOf(coinType, tx.value),
-                            Value.valueOf(coinType, tx.gasPrice), tx.confirmations)
+                    toAdd.forEach { tx ->
+                        backing.putTransaction(tx.blockNumber, tx.timeStamp, tx.hash,
+                                "", tx.from, tx.to, Value.valueOf(coinType, tx.value),
+                                Value.valueOf(coinType, tx.gasPrice), tx.confirmations)
+                    }
+                    toUpdate.forEach { tx ->
+                        backing.updateTransaction(tx.hash, tx.blockNumber, tx.confirmations)
+                    }
+                    toDelete.forEach { id ->
+                        backing.deleteTransaction(id)
+                    }
+                    callback()
+                } catch (e: Exception) {
+                    logger.log(Level.SEVERE, "Failed to reach server: ${e.localizedMessage}")
                 }
-                toUpdate.forEach { tx ->
-                    backing.updateTransaction(tx.hash, tx.blockNumber, tx.confirmations)
-                }
-                toDelete.forEach { id ->
-                    backing.deleteTransaction(id)
-                }
-                callback()
             }
         }
     }
@@ -55,6 +61,7 @@ object EtherscanIoFetcher {
     private const val PRODNET_SUBDOMAIN = "api"
     private const val TESTNET_SUBDOMAIN = "api-ropsten"
     private val TIME_WAIT_FOR_CONFIRMATION = TimeUnit.DAYS.toSeconds(14)
+    private val logger = Logger.getLogger(this::javaClass.name)
 }
 
 class EtherscanApiTxlist {
