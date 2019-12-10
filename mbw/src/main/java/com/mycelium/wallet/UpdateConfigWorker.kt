a4b7f7bb5586188d60e986012e9fc01ec056e00e
Leo Wandersleb <leo@leowandersleb.de>
2019-12-10 20:02:19 +1300

    stop WorkerManager when app terminates

--- mbw/src/main/java/com/mycelium/wallet/UpdateConfigWorker.kt
+++ mbw/src/main/java/com/mycelium/wallet/UpdateConfigWorker.kt
@@ -6,27 +6,39 @@ import java.util.concurrent.TimeUnit
 
 private const val WORK_NAME_PERIODIC = "configupdate-periodic"
 
-fun start(context: Context) {
-    val workManager = WorkManager.getInstance(context);
-    val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.CONNECTED)
-            .build()
-    val workRequest = PeriodicWorkRequest.Builder(UpdateConfigWorker::class.java, Constants.CONFIG_UPDATE_PERIOD_MINS, TimeUnit.MINUTES)
-            .setInitialDelay(Constants.CONFIG_UPDATE_PERIOD_MINS, TimeUnit.MINUTES)
-            .setConstraints(constraints)
-            .build()
-    workManager.enqueueUniquePeriodicWork(WORK_NAME_PERIODIC, ExistingPeriodicWorkPolicy.REPLACE, workRequest);
-}
-
 class UpdateConfigWorker(val context: Context, params: WorkerParameters) : Worker(context, params) {
     override fun doWork(): Result {
         return try {
-            if (MbwManager.getInstance(context).isAppInForeground) {
-                MbwManager.getInstance(applicationContext).updateConfig()
+            MbwManager.getInstance(context).run {
+                if (isAppInForeground) {
+                    updateConfig()
+                }
             }
             Result.success()
         } catch (throwable: Throwable) {
             Result.failure()
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun start(context: Context) {
+            val workManager = WorkManager.getInstance(context)
+            val constraints = Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.CONNECTED)
+                    .build()
+            val workRequest = PeriodicWorkRequest
+                    .Builder(UpdateConfigWorker::class.java, Constants.CONFIG_UPDATE_PERIOD_MINS, TimeUnit.MINUTES)
+                    .setInitialDelay(Constants.CONFIG_UPDATE_PERIOD_MINS, TimeUnit.MINUTES)
+                    .setConstraints(constraints)
+                    .build()
+            workManager.enqueueUniquePeriodicWork(WORK_NAME_PERIODIC, ExistingPeriodicWorkPolicy.REPLACE, workRequest)
+        }
+
+        @JvmStatic
+        fun end(context: Context) {
+            val workManager = WorkManager.getInstance(context)
+            workManager.cancelAllWork()
         }
     }
 }
