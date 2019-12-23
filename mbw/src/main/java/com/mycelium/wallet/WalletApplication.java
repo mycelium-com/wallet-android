7fd72661845da1d60885fa84318324d4c55b9291
agneslovelace <hamman12386@gmail.com>
2019-12-23 22:09:47 +0300

    Remove unnecessary checks in WalletApplication

--- mbw/src/main/java/com/mycelium/wallet/WalletApplication.java
+++ mbw/src/main/java/com/mycelium/wallet/WalletApplication.java
@@ -81,15 +81,6 @@ public class WalletApplication extends MultiDexApplication implements ModuleMess
 
     @Override
     public void onCreate() {
-        final Provider provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME);
-        if (provider == null) {
-            // Web3j will set up the provider lazily when it's first used.
-            return;
-        }
-        if (provider.getClass().equals(BouncyCastleProvider.class)) {
-            // BC with same package name, shouldn't happen in real life.
-            return;
-        }
         // Android registers its own BC provider. As it might be outdated and might not include
         // all needed ciphers, we substitute it with a known BC bundled in the app.
         // Android's BC has its package rewritten to "com.android.org.bouncycastle" and because
