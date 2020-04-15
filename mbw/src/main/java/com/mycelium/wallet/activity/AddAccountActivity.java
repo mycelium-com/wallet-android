7b0669b7a8c642a95c45ac08e620c1b00fd99e8c
kurtnebiev-elvis <60182893+kurtnebiev-elvis@users.noreply.github.com>
2020-04-15 13:42:56 +0300

    Update mbw/src/main/java/com/mycelium/wallet/activity/AddAccountActivity.java
    Co-Authored-By: agneslovelace <hamman12386@gmail.com>

--- mbw/src/main/java/com/mycelium/wallet/activity/AddAccountActivity.java
+++ mbw/src/main/java/com/mycelium/wallet/activity/AddAccountActivity.java
@@ -303,7 +303,7 @@ public class AddAccountActivity extends Activity {
     private class ETHCreationAsyncTask extends AsyncTask<Void, Integer, UUID> {
         List<ERC20Token> tokens;
 
-        ETHCreationAsyncTask(@Nullable List<ERC20Token> token) {
+        ETHCreationAsyncTask(@Nullable List<ERC20Token> tokens) {
             this.token = token;
         }
 
