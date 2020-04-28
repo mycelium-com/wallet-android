55cd1564769baf87d58d06299ca03fffcefab2c2
mikhailz <zisman.mikhail@gmail.com>
2020-04-28 18:11:57 +0300

    added timer

--- mbw/src/main/java/com/mycelium/bequant/kyc/checkCode/VerifyPhoneFragment.kt
+++ mbw/src/main/java/com/mycelium/bequant/kyc/checkCode/VerifyPhoneFragment.kt
@@ -6,13 +6,17 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mycelium.wallet.R
+import kotlinx.android.synthetic.main.activity_bequant_kyc_verify_phone.*
 
 class VerifyPhoneFragment: Fragment(R.layout.activity_bequant_kyc_verify_phone){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+            pinCode.setText("555555")
+        },2000)
 
         Handler().postDelayed({
             findNavController().navigate(R.id.action_phoneVerifyToStep1)
-        },5000)
+        },3000)
     }
 }
