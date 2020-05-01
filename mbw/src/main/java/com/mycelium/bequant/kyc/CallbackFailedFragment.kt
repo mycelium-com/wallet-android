b368a357f43b78487d5a4abff59345f4db0705ee
mikhailz <zisman.mikhail@gmail.com>
2020-05-01 14:02:46 +0300

    added CallbackFailedFragment, not connected yet

---
+++ mbw/src/main/java/com/mycelium/bequant/kyc/CallbackFailedFragment.kt
@@ -0,0 +1,38 @@
+package com.mycelium.bequant.kyc
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.mycelium.wallet.R
+import kotlinx.android.synthetic.main.fragment_country_fail.*
+
+class CallbackFailedFragment : Fragment(R.layout.fragment_kyc_callback_failed) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity?)?.supportActionBar?.title = "Identity Authentication "
+        (activity as AppCompatActivity?)?.supportActionBar?.setHomeAsUpIndicator(resources.getDrawable(R.drawable.ic_bequant_arrow_back))
+        backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        finishButton.setOnClickListener {
+            findNavController().navigate(CallbackFailedFragmentDirections.actionClose())
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+            when (item.itemId) {
+                android.R.id.home -> {
+                    activity?.onBackPressed()
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+}
