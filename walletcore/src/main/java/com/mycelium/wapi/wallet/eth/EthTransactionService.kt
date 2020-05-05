edb1f13b9ebcf735ede64f100996a12587070b18
agneslovelace <hamman12386@gmail.com>
2020-05-05 16:40:38 +0300

    Calculate balance based on tx history for eth/erc20 plus small improv.

--- walletcore/src/main/java/com/mycelium/wapi/wallet/eth/EthTransactionService.kt
+++ walletcore/src/main/java/com/mycelium/wapi/wallet/eth/EthTransactionService.kt
@@ -2,8 +2,18 @@ package com.mycelium.wapi.wallet.eth
 
 import com.mycelium.net.HttpsEndpoint
 
-class EthTransactionService(address: String, transactionServiceEndpoints: List<HttpsEndpoint>)
+class EthTransactionService(private val address: String, transactionServiceEndpoints: List<HttpsEndpoint>)
     : AbstractTransactionService(address, transactionServiceEndpoints) {
 
-    override fun getTransactions() = fetchTransactions().filter { it.tokenTransfers.isEmpty() }
+    override fun getTransactions() = fetchTransactions()
+            .filter {
+                it.tokenTransfers.isEmpty() ||
+                        (it.tokenTransfers.isNotEmpty() && it.tokenTransfers.any { transfer ->
+                            isOutgoing(transfer)
+                        })
+            }
+
+    private fun isOutgoing(transfer: TokenTransfer) =
+            transfer.from.equals(address, true) && !transfer.to.equals(address, true) ||
+                    transfer.from.equals(address, true) && transfer.to.equals(address, true)
 }
