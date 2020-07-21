91e670d46e98af87e3942bd2bde6cd67d064600a
agneslovelace <hamman12386@gmail.com>
2020-07-21 17:40:17 +0300

    Pretify error notifications
    also: remove enums from ErrorError as they are seems to be outdated comparing to docs on the bequant site

--- mbw/src/main/java/com/mycelium/bequant/common/ErrorHandler.kt
+++ mbw/src/main/java/com/mycelium/bequant/common/ErrorHandler.kt
@@ -2,13 +2,19 @@ package com.mycelium.bequant.common
 
 import android.app.AlertDialog
 import android.content.Context
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import com.mycelium.bequant.remote.trading.model.Error
 
 
 class ErrorHandler(val context: Context) {
-    fun handle(error: String) {
+    fun handle(errorPayload: String) {
+        val mapper = ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false).registerKotlinModule()
+        val error = mapper.readValue(errorPayload, Error::class.java).error!!
         AlertDialog.Builder(context)
                 .setTitle("Error")
-                .setMessage(error)
+                .setMessage("${error.message}. ${error.description}")
                 .create()
                 .show()
     }
