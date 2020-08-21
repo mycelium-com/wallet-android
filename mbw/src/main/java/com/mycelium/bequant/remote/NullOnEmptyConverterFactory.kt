b8da81f8b667b2b48c6154ad090dd000e820ae13
Leo Wandersleb <leo.wandersleb@mycelium.com>
2020-08-20 22:55:44 -0400

    cleanups

--- mbw/src/main/java/com/mycelium/bequant/remote/NullOnEmptyConverterFactory.kt
+++ mbw/src/main/java/com/mycelium/bequant/remote/NullOnEmptyConverterFactory.kt
@@ -7,8 +7,11 @@ import java.lang.reflect.Type
 
 
 class NullOnEmptyConverterFactory : Converter.Factory() {
-    override fun responseBodyConverter(type: Type, annotations: Array<Annotation>, retrofit: Retrofit): Converter<ResponseBody, *>? {
+    override fun responseBodyConverter(type: Type,
+                                       annotations: Array<Annotation>,
+                                       retrofit: Retrofit): Converter<ResponseBody, *>? {
         val delegate = retrofit.nextResponseBodyConverter<Any?>(this, type, annotations)
-        return Converter<ResponseBody, Any> { body -> if (body.contentLength() == 0L) null else delegate.convert(body) }
+        return Converter<ResponseBody, Any> { body ->
+            if (body.contentLength() == 0L) null else delegate.convert(body) }
     }
 }
--- mbw/src/main/java/com/mycelium/bequant/sign/SignActivity.kt
+++ mbw/src/main/java/com/mycelium/bequant/sign/SignActivity.kt
@@ -7,11 +7,9 @@ import androidx.lifecycle.lifecycleScope
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.mycelium.bequant.Constants.ACTION_BEQUANT_EMAIL_CONFIRMED
 import com.mycelium.bequant.Constants.ACTION_BEQUANT_RESET_PASSWORD_CONFIRMED
-import com.mycelium.bequant.Constants.ACTION_BEQUANT_TOTP_CONFIRMED
 import com.mycelium.bequant.common.ErrorHandler
 import com.mycelium.bequant.common.loader
 import com.mycelium.bequant.remote.repositories.Api
-import com.mycelium.bequant.remote.repositories.SignRepository
 import com.mycelium.wallet.R
 import kotlinx.android.synthetic.main.activity_bequant_sign.*
 
@@ -43,15 +41,18 @@ class SignActivity : AppCompatActivity(R.layout.activity_bequant_sign) {
                     success = {
                         LocalBroadcastManager.getInstance(this).sendBroadcast(Intent(ACTION_BEQUANT_EMAIL_CONFIRMED))
                     },
-                    error = { code, message ->
+                    error = { _, message ->
                         ErrorHandler(this).handle(message)
-                    }, finally = {
-                loader(false)
-            })
+                    },
+                    finally = {
+                        loader(false)
+                    })
         } else if (intent.action == Intent.ACTION_VIEW
                 && intent.data?.host == "reg.bequant.io"
                 && intent.data?.path == "/account/password/set") {
-            LocalBroadcastManager.getInstance(this).sendBroadcast(Intent(ACTION_BEQUANT_RESET_PASSWORD_CONFIRMED)
+            LocalBroadcastManager
+                    .getInstance(this)
+                    .sendBroadcast(Intent(ACTION_BEQUANT_RESET_PASSWORD_CONFIRMED)
                     .putExtra("token", intent.data?.getQueryParameter("token") ?: ""))
         }
     }
