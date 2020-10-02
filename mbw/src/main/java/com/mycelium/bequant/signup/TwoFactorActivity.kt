428e6b4d61ea2b50ef3cfff2d02b46da66399fe7
itserg <itserg2008@gmail.com>
2020-10-02 20:30:35 +0300

    Two-factor activity is changed to Firebase dynamic links

--- mbw/src/main/java/com/mycelium/bequant/signup/TwoFactorActivity.kt
+++ mbw/src/main/java/com/mycelium/bequant/signup/TwoFactorActivity.kt
@@ -1,16 +1,21 @@
 package com.mycelium.bequant.signup
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import com.google.firebase.dynamiclinks.ktx.dynamicLinks
+import com.google.firebase.ktx.Firebase
+import com.mycelium.bequant.Constants
 import com.mycelium.bequant.Constants.ACTION_BEQUANT_TOTP_CONFIRMED
 import com.mycelium.bequant.common.ErrorHandler
 import com.mycelium.bequant.common.loader
 import com.mycelium.bequant.remote.repositories.Api
 import com.mycelium.wallet.R
 import kotlinx.android.synthetic.main.activity_two_factor.*
+import org.json.JSONObject
 
 
 class TwoFactorActivity : AppCompatActivity(R.layout.activity_two_factor) {
@@ -22,30 +27,40 @@ class TwoFactorActivity : AppCompatActivity(R.layout.activity_two_factor) {
             setDisplayShowTitleEnabled(true)
             setHomeAsUpIndicator(resources.getDrawable(R.drawable.ic_back_arrow))
         }
-        handleIntent(intent)
+        handleDeepLink(intent)
     }
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
-        handleIntent(intent)
+        handleDeepLink(intent)
     }
 
-    private fun handleIntent(intent: Intent) {
-        if (intent.action == Intent.ACTION_VIEW
-                && intent.data?.host == "reg.bequant.io"
-                && intent.data?.path == "/account/totp/confirm") {
-            loader(true)
-            Api.signRepository.accountTotpConfirm(lifecycleScope,
-                    token = intent.data?.getQueryParameter("token") ?: "",
-                    success = {
-                        LocalBroadcastManager.getInstance(this).sendBroadcast(Intent(ACTION_BEQUANT_TOTP_CONFIRMED))
-                    },
-                    error = { _, message ->
-                        ErrorHandler(this).handle(message)
-                    },
-                    finally = {
-                        loader(false)
-                    })
-        }
+    private fun handleDeepLink(intent:Intent) {
+        Firebase.dynamicLinks
+                .getDynamicLink(intent)
+                .addOnSuccessListener(this) { pendingDynamicLinkData ->
+                    var deepLink: Uri? = null
+                    if (pendingDynamicLinkData != null) {
+                        deepLink = pendingDynamicLinkData.link
+                    }
+
+                    if (deepLink != null) {
+                        if (deepLink.host == "reg.bequant.io" && deepLink.path == "/account/totp/confirm") {
+                            loader(true)
+                            Api.signRepository.accountTotpConfirm(lifecycleScope,
+                                    token = intent.data?.getQueryParameter("token") ?: "",
+                                    success = {
+                                        LocalBroadcastManager.getInstance(this).sendBroadcast(Intent(ACTION_BEQUANT_TOTP_CONFIRMED))
+                                    },
+                                    error = { _, message ->
+                                        ErrorHandler(this).handle(message)
+                                    },
+                                    finally = {
+                                        loader(false)
+                                    })
+                        }
+                    }
+                }
     }
+
 }
