631c4125c8f806a98a06e9323675d436bdf1dde5
itserg <itserg2008@gmail.com>
2020-10-14 19:27:08 +0300

    Fix group name

--- walletcore/src/main/java/com/mycelium/wapi/wallet/fio/FioGroup.kt
+++ walletcore/src/main/java/com/mycelium/wapi/wallet/fio/FioGroup.kt
@@ -5,7 +5,7 @@ import fiofoundation.io.fiosdk.models.fionetworkprovider.FIORequestContent
 class FioGroup(var status: Type, val children: MutableList<FIORequestContent> = mutableListOf()) {
     enum class Type(private val s: String) {
         SENT("SENT FIO REQUESTS"),
-        PENDING("RECEIVED FIO REQUESTS");
+        PENDING("PENDING FIO REQUESTS");
 
         override fun toString(): String {
             return s
