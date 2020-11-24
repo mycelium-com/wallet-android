d970834c9e2dc1e23b8070778b900d63099d07b6
itserg <itserg2008@gmail.com>
2020-11-24 09:46:25 +0300

    Connectivity status change logging

--- mbw/src/main/java/com/mycelium/wallet/NetworkChangedReceiver.kt
+++ mbw/src/main/java/com/mycelium/wallet/NetworkChangedReceiver.kt
@@ -3,10 +3,14 @@ package com.mycelium.wallet
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import java.util.logging.Level
+import java.util.logging.LogRecord
 
 class NetworkChangedReceiver : BroadcastReceiver() {
     // We receive this event on wallet start, but this would start heavy init, which we don't want to.
     var wasInited: Boolean = false
+
+    val logger = java.util.logging.Logger.getLogger(com.mycelium.wapi.wallet.btc.AbstractBtcAccount::class.java.getSimpleName())
 
     override fun onReceive(context: Context, intent: Intent) {
         if (!wasInited) {
@@ -16,6 +20,7 @@ class NetworkChangedReceiver : BroadcastReceiver() {
         if (intent.action == "android.net.conn.CONNECTIVITY_CHANGE") {
             val mbwManager = MbwManager.getInstance(context)
             val connected = Utils.isConnected(context)
+            logger.log(Level.INFO,"Connectivity status has been changed. Connected: ${connected}")
             mbwManager.getWalletManager(false).isNetworkConnected = connected
             mbwManager.wapi.setNetworkConnected(connected)
         }
