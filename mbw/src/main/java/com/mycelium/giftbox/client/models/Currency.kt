2961197dcd2cb40f2e4f87ea7232e682750410ef
elvis <xElvis89x@gmail.com>
2021-05-25 02:18:54 +0300

    fix project can't build

--- mbw/src/main/java/com/mycelium/giftbox/client/models/Currency.kt
+++ mbw/src/main/java/com/mycelium/giftbox/client/models/Currency.kt
@@ -5,7 +5,7 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Currency(
-    val extraIdName: Any,
+    val extraIdName: String?,
     val fullName: String?,
     val name: String?
 ) : Parcelable
