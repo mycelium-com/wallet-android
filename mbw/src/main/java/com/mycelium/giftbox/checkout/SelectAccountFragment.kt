6fde2ef46bbb3af1c3660d6f79d81d255c122781
elvis <xElvis89x@gmail.com>
2021-05-25 21:24:32 +0300

    refactoring

--- mbw/src/main/java/com/mycelium/giftbox/checkout/SelectAccountFragment.kt
+++ mbw/src/main/java/com/mycelium/giftbox/checkout/SelectAccountFragment.kt
@@ -12,7 +12,7 @@ import com.mycelium.bequant.receive.adapter.AccountListItem
 import com.mycelium.wallet.MbwManager
 import com.mycelium.wallet.R
 import com.mycelium.wallet.activity.util.getBTCSingleAddressAccounts
-import com.mycelium.wallet.exchange.ValueSum
+import com.mycelium.wallet.activity.util.getSpendableBalance
 import com.mycelium.wapi.wallet.btc.bip44.getBTCBip44Accounts
 import com.mycelium.wapi.wallet.eth.getEthAccounts
 import kotlinx.android.synthetic.main.fragment_bequant_select_account.*
@@ -38,16 +38,15 @@ class SelectAccountFragment : Fragment(R.layout.fragment_giftbox_select_account)
         val accountsList = mutableListOf<AccountListItem>()
         walletsAccounts.forEach {
             if (it.second.isNotEmpty()) {
-                accountsList.add(AccountGroupItem(true, getString(it.first), ValueSum()))
+                accountsList.add(AccountGroupItem(true, getString(it.first), it.second.getSpendableBalance()))
                 accountsList.addAll(it.second.map { AccountItem(it.label, it.accountBalance.confirmed) })
             }
         }
-//        accountsList.add(TotalItem(getSpendableBalance()))
         adapter.submitList(accountsList)
 
         adapter.accountClickListener = { accountItem ->
             val selectedAccount = walletsAccounts.map { it.second }.flatten().find { it.label == accountItem.label }
-            findNavController().navigate(SelectAccountFragmentDirections.actionCheckout(args.product, selectedAccount?.id!!,100, 0))
+            findNavController().navigate(SelectAccountFragmentDirections.actionCheckout(args.product, selectedAccount?.id!!, 100, 0))
         }
     }
 }
