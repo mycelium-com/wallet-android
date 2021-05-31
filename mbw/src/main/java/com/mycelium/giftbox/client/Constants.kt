d40d39f030c7003cf37cbcfda2723e8b745dda4c
mikhailz <zisman.mikhail@gmail.com>
2021-05-31 16:26:27 +0300

    refactoring

--- mbw/src/main/java/com/mycelium/giftbox/client/Constants.kt
+++ mbw/src/main/java/com/mycelium/giftbox/client/Constants.kt
@@ -1,13 +1,10 @@
 package com.mycelium.giftbox.client
 
 object Constants {
-    const val ACCESS_TOKEN_KEY: String = "ACCESS_TOKEN_KEY"
     const val ENDPOINT = "https://r6lnk2g9ki.execute-api.us-east-1.amazonaws.com/prd/"
 //    "https://apps-api.giftbox.tech/api/"
 
     //TEST DATA
     const val CLIENT_ORDER_ID = "test-order-217"
-    const val CLIENT_USER_ID = "test-user"
-    const val PRODUCT_ID = "AGIFT-HP"
 
 }
