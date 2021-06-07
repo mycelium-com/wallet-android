39ef2a89389f55b2bf403345f0e77fc77ff0415e
elvis <xElvis89x@gmail.com>
2021-06-07 16:23:42 +0300

    giftbox store list

--- mbw/src/main/java/com/mycelium/giftbox/cards/StoresFragment.kt
+++ mbw/src/main/java/com/mycelium/giftbox/cards/StoresFragment.kt
@@ -12,8 +12,8 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.tabs.TabLayout
 import com.mycelium.bequant.kyc.inputPhone.coutrySelector.CountriesSource
-import com.mycelium.giftbox.cards.adapter.SearchTagAdapter
 import com.mycelium.giftbox.cards.adapter.StoresAdapter
 import com.mycelium.giftbox.cards.viewmodel.GiftBoxViewModel
 import com.mycelium.giftbox.cards.viewmodel.StoresViewModel
@@ -22,11 +22,11 @@ import com.mycelium.wallet.R
 import com.mycelium.wallet.activity.modern.Toaster
 import com.mycelium.wallet.activity.view.DividerItemDecoration
 import com.mycelium.wallet.databinding.FragmentGiftboxStoresBinding
+import kotlinx.android.synthetic.main.media_flow_tab_item.view.*
 
 
 class StoresFragment : Fragment() {
 
-    private val tagsAdapter = SearchTagAdapter()
     private val adapter = StoresAdapter()
     private val viewModel: StoresViewModel by viewModels()
     private val activityViewModel: GiftBoxViewModel by activityViewModels()
@@ -47,13 +47,32 @@ class StoresFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding?.tags?.adapter = tagsAdapter
-        tagsAdapter.clickListener = {
-            viewModel.category = if (it == "All") null else it
-            loadData()
-        }
-        activityViewModel.categories.observe(viewLifecycleOwner) {
-            tagsAdapter.submitList(listOf("All") + it)
+        binding?.tags?.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                viewModel.category = if (tab.tag == "All") null else tab.tag.toString()
+                loadData()
+            }
+        })
+        activityViewModel.categories.observe(viewLifecycleOwner) { categories ->
+            binding?.tags?.let { tags ->
+                categories.forEach {
+                    if (getTab(it, tags) == null) {
+                        val tab = tags.newTab().setCustomView(
+                                layoutInflater.inflate(R.layout.media_flow_tab_item, tags, false).apply {
+                                    this.text.text = it.replace("-", " ")
+                                })
+                        tab.tag = it
+                        tags.addTab(tab)
+                    }
+                }
+                cleanTabs(categories, tags)
+            }
         }
         binding?.list?.adapter = adapter
         binding?.list?.itemAnimator = null
@@ -94,7 +113,8 @@ class StoresFragment : Fragment() {
                 country = activityViewModel.selectedCountries.value,
                 offset = offset, limit = 30,
                 success = {
-                    activityViewModel.categories.value = it?.categories
+                    activityViewModel.categories.value = listOf("All") + (it?.categories
+                            ?: emptyList())
                     activityViewModel.countries.value = it?.countries?.mapNotNull {
                         CountriesSource.countryModels.find { model -> model.acronym.equals(it, true) }
                     }
@@ -138,5 +158,22 @@ class StoresFragment : Fragment() {
         binding?.list?.clearOnScrollListeners()
         binding = null
         super.onDestroyView()
+    }
+
+    private fun getTab(category: String, tabLayout: TabLayout): TabLayout.Tab? {
+        for (i in 0 until tabLayout.tabCount) {
+            if (tabLayout.getTabAt(i)?.tag == category) {
+                return tabLayout.getTabAt(i)
+            }
+        }
+        return null
+    }
+
+    private fun cleanTabs(list: List<String>, tabLayout: TabLayout) {
+        for (i in tabLayout.tabCount - 1 downTo 0) {
+            if (!list.contains(tabLayout.getTabAt(i)?.tag)) {
+                tabLayout.removeTab(tabLayout.getTabAt(i)!!)
+            }
+        }
     }
 }
