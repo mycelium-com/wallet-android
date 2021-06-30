d69c372a48d9cc27cd5a78658557a80d6f53e02e
elvis <xElvis89x@gmail.com>
2021-06-30 15:36:14 +0300

    #3215 giftbox share

--- mbw/src/main/java/com/mycelium/giftbox/Bindings.kt
+++ mbw/src/main/java/com/mycelium/giftbox/Bindings.kt
@@ -5,10 +5,12 @@ import android.content.res.Resources
 import android.webkit.URLUtil
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.mycelium.giftbox.client.models.Order
+import com.mycelium.wallet.R
 import java.text.DateFormat
 import java.util.*
 
@@ -28,31 +30,31 @@ fun Date.getDateTimeString(resources: Resources): String =
                 DateFormat.getTimeInstance(DateFormat.SHORT, resources.configuration.locale).format(this)
 
 fun TextView.setupDescription(description: String, more: Boolean): Unit {
-    text = androidx.core.text.HtmlCompat.fromHtml(description, androidx.core.text.HtmlCompat.FROM_HTML_MODE_LEGACY)
+    text = HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_LEGACY)
     if (!more && layout != null) {
         val endIndex = layout.getLineEnd(3) - 3
         if (0 < endIndex && endIndex < description.length - 3) {
-            text = androidx.core.text.HtmlCompat.fromHtml("${description.subSequence(0, endIndex)}...", androidx.core.text.HtmlCompat.FROM_HTML_MODE_LEGACY)
+            text = HtmlCompat.fromHtml("${description.subSequence(0, endIndex)}...", HtmlCompat.FROM_HTML_MODE_LEGACY)
         }
     }
 }
 
-fun Order.shareText(): String {
-    var text = ""
+fun Order.shareText(resources: Resources): String {
+    var text = resources.getString(R.string.share_gift_card_text, productName, amount, currencyCode)
     val code = items?.firstOrNull()
     when {
         code?.deliveryUrl?.isNotEmpty() == true -> {
-            text += code.deliveryUrl
+            text += "\nUrl: " + code.deliveryUrl
         }
         URLUtil.isValidUrl(code?.code) -> {
-            text += code?.code
+            text += "\nUrl: " + code?.code
         }
         code?.code?.isNotEmpty() == true -> {
-            text += code.code
+            text += "\nCode: " + code.code
         }
     }
     if (code?.pin?.isNotEmpty() == true) {
-        text += "\n" + code.pin
+        text += "\nPin: " + code.pin
     }
     return text
 }
@@ -61,8 +63,8 @@ fun Fragment.shareGiftcard(order: Order) {
     startActivity(
             Intent.createChooser(
                     Intent(Intent.ACTION_SEND)
-                            .putExtra(Intent.EXTRA_SUBJECT, "Gift Card")
-                            .putExtra(Intent.EXTRA_TEXT, order.shareText())
+                            .putExtra(Intent.EXTRA_SUBJECT, "Gift Card information")
+                            .putExtra(Intent.EXTRA_TEXT, order.shareText(resources))
                             .setType("text/plain"), "share gift card"
             )
     )
