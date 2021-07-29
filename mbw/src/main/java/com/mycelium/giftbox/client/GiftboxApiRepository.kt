4fdc3ef0e154bdc5fd931cc8ba34adc46ef02604
elvis <xElvis89x@gmail.com>
2021-07-29 12:53:33 +0300

    fix giftbox code

--- mbw/src/main/java/com/mycelium/giftbox/client/GiftboxApiRepository.kt
+++ mbw/src/main/java/com/mycelium/giftbox/client/GiftboxApiRepository.kt
@@ -14,7 +14,6 @@ import com.squareup.sqldelight.android.AndroidSqliteDriver
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import retrofit2.Response
-import java.math.BigDecimal
 import java.util.*
 
 class GiftboxApiRepository {
@@ -25,10 +24,10 @@ class GiftboxApiRepository {
              GiftboxCard.Adapter(dateAdapter))
 
     private val clientUserIdFromMasterSeed by lazy {
-        val mbwManager = MbwManager.getInstance(WalletApplication.getInstance())
-        mbwManager.masterSeedManager.getIdentityAccountKeyManager(AesKeyCipher.defaultKeyCipher())
-            .getPrivateKeyForWebsite(Constants.WEBSITE, AesKeyCipher.defaultKeyCipher())
-            .publicKey.toString()
+        MbwManager.getInstance(WalletApplication.getInstance())
+                .masterSeedManager.getIdentityAccountKeyManager(AesKeyCipher.defaultKeyCipher())
+                .getPrivateKeyForWebsite(Constants.WEBSITE, AesKeyCipher.defaultKeyCipher())
+                .publicKey.toString()
     }
 
     private fun updateOrderId(): String {
@@ -49,7 +48,7 @@ class GiftboxApiRepository {
         doRequest(scope, {
             api.price(
                 clientUserIdFromMasterSeed,
-                lastOrderId!!,
+                lastOrderId,
                 amount,
                 quantity,
                 code,
@@ -68,7 +67,7 @@ class GiftboxApiRepository {
         return doRequest(scope, {
             api.product(
                 clientUserIdFromMasterSeed,
-                Constants.CLIENT_ORDER_ID, //TODO check why client order id need for product
+                lastOrderId,
                 productId
             )
         }, successBlock = success, errorBlock = error, finallyBlock = finally)
