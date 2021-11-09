862350ed9d1c3ac611d3a48db96e0572864ec4c5
itserg <itserg2008@gmail.com>
2021-11-09 11:36:08 +0300

    Removed indication for account interruptions

--- mbw/src/main/java/com/mycelium/wallet/activity/modern/model/accounts/AccountInvestmentViewModel.kt
+++ mbw/src/main/java/com/mycelium/wallet/activity/modern/model/accounts/AccountInvestmentViewModel.kt
@@ -8,7 +8,7 @@ import com.mycelium.wapi.wallet.WalletAccount
 class AccountInvestmentViewModel(val account: WalletAccount<out Address>, val balance: String) : AccountListItem, SyncStatusItem {
     val accountId = account.id!!
     var label = "Trading Account"
-    override var isSyncError = account.lastSyncStatus()?.status in arrayOf(SyncStatus.INTERRUPT, SyncStatus.ERROR)
+    override var isSyncError = account.lastSyncStatus()?.status == SyncStatus.ERROR
 
     override fun getType(): AccountListItem.Type = AccountListItem.Type.INVESTMENT_TYPE
 }
