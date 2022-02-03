9fc2718b924b640c599865d489bd98e34c72abd7
elvis <xElvis89x@gmail.com>
2022-02-03 13:48:51 +0300

    #rm3980 fix long eth synchronization

--- walletcore/src/main/java/com/mycelium/wapi/wallet/eth/EthAccount.kt
+++ walletcore/src/main/java/com/mycelium/wapi/wallet/eth/EthAccount.kt
@@ -186,13 +186,7 @@ class EthAccount(private val chainId: Byte,
     private fun syncTransactions(): Boolean {
         try {
             val remoteTransactions = blockchainService.getTransactions(receivingAddress.addressString)
-            remoteTransactions.forEach { tx ->
-                backing.putTransaction(tx.blockHeight.toInt(), tx.blockTime, tx.txid, "", tx.from, tx.to,
-                        valueOf(coinType, tx.value), valueOf(coinType, tx.gasPrice * (tx.gasUsed
-                        ?: typicalEstimatedTransactionSize.toBigInteger())), tx.confirmations.toInt(),
-                        tx.nonce,  valueOf(coinType, tx.internalValue ?: BigInteger.ZERO),
-                        tx.success, tx.gasLimit, tx.gasUsed)
-            }
+            backing.putTransactions(remoteTransactions, coinType, typicalEstimatedTransactionSize.toBigInteger())
             val localTxs = getUnconfirmedTransactions()
             // remove such transactions that are not on server anymore
             // this could happen if transaction was replaced by another e.g.
