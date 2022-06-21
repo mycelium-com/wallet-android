e0e7f4f24889ae4e36ffb07affa5ad0412c4a574
elvis <xElvis89x@gmail.com>
2022-06-21 13:24:29 +0300

    fix exchange cahngelly2

--- mbw/src/main/java/com/mycelium/wallet/external/changelly2/ExchangeFragment.kt
+++ mbw/src/main/java/com/mycelium/wallet/external/changelly2/ExchangeFragment.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import android.view.animation.Animation
 import android.view.animation.LinearInterpolator
