3e213a98ff70d770d3e871151d4890d1a28645cf
elvis <xElvis89x@gmail.com>
2022-08-10 13:28:02 +0300

    taproot

--- bitlib/src/test/java/com/mrd/bitlib/TaprootTransactionTest.kt
+++ bitlib/src/test/java/com/mrd/bitlib/TaprootTransactionTest.kt
@@ -2,27 +2,43 @@ package com.mrd.bitlib
 
 import com.mrd.bitlib.crypto.InMemoryPrivateKey
 import com.mrd.bitlib.crypto.PublicKey
+import com.mrd.bitlib.model.AddressType
+import com.mrd.bitlib.model.BitcoinAddress
+import com.mrd.bitlib.model.NetworkParameters
 import com.mrd.bitlib.util.HexUtils
 import org.junit.Assert
 import org.junit.Test
 
 
 // https://github.com/bitcoin-core/btcdeb/blob/master/doc/tapscript-example-with-tap.md
-//bc1p5cyxnuxmeuwuvkwfem96lqzszd02n6xdcjrs20cac6yqjjwudpxqkedrcr
-//bc1pq0xg5j7xfkyhhhw9l0p0vu8h4zaqkwr80ygxeufz83hut47ddlq32dnksmj
-//bc1pqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqsyjer9e
+
 
 class TaprootTransactionTest {
-    val privateKey = InMemoryPrivateKey(HexUtils.toBytes("cc8a4bc64d897bddc5fbc2f670f7a8ba0b386779106cf1223c6fc5d7cd6fc115"))
-    val publicKey = PublicKey(HexUtils.toBytes("a60869f0dbcf1dc659c9cecbaf8050135ea9e8cdc487053f1dc6880949dc684c"))
 
-//    val publicKey = PublicKey(HexUtils.toBytes("03ad1d8e89212f0b92c74d23bb710c00662ad1470198ac48c43f7d6f93a2a26873"))
-//    val privateKey = InMemoryPrivateKey(HexUtils.toBytes("eb696a065ef48a2192da5b28b694f87544b30fae8327c4510137a922f32c6dcf"))
+    //mnemonic = abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about
+    // Account 0, first receiving address = m/84'/0'/0'/0/0
+    val bip84PrivateKey = InMemoryPrivateKey("KyZpNDKnfs94vbrwhJneDi77V6jF64PWPF8x5cdJb8ifgg2DUc9d",
+            NetworkParameters.productionNetwork)
+    val bip84PublicKey = PublicKey(HexUtils.toBytes("0330d54fd0dd420a6e5f8d3624f5f3482cae350f79d5f0753bf5beef9c2d91af3c"))
+    val bip84Address = BitcoinAddress.fromString("bc1qcr8te4kr609gcawutmrza0j4xv80jy8z306fyu")
+
+    // Account 0, first receiving address = m/86'/0'/0'/0/0
+    val bip86PrivateKey = InMemoryPrivateKey("KyRv5iFPHG7iB5E4CqvMzH3WFJVhbfYK4VY7XAedd9Ys69mEsPLQ",
+            NetworkParameters.productionNetwork)
+    val bip86PublicKey = PublicKey(HexUtils.toBytes("03cc8a4bc64d897bddc5fbc2f670f7a8ba0b386779106cf1223c6fc5d7cd6fc115"))
+    val bip86Address = BitcoinAddress.fromString("bc1p5cyxnuxmeuwuvkwfem96lqzszd02n6xdcjrs20cac6yqjjwudpxqkedrcr")
 
     @Test
-    fun checkKeys() {
-//        Assert.assertArrayEquals(publicKey.publicKeyBytes, privateKey.publicKey.publicKeyBytes)
-        Assert.assertArrayEquals(publicKey.publicKeyBytes, privateKey.publicKey.pubKeyCompressed)
+    fun testBip84Keys() {
+        Assert.assertEquals(bip84PrivateKey.publicKey.toAddress(NetworkParameters.productionNetwork, AddressType.P2WPKH), bip84Address)
+        Assert.assertArrayEquals(bip84PrivateKey.publicKey.publicKeyBytes, bip84PublicKey.publicKeyBytes)
+    }
+
+    @Test
+    fun testBip86Keys() {
+        Assert.assertArrayEquals(bip86PrivateKey.publicKey.publicKeyBytes, bip86PublicKey.publicKeyBytes)
+        Assert.assertEquals(bip86Address,
+                bip86PrivateKey.publicKey.toAddress(NetworkParameters.productionNetwork, AddressType.P2TR, true))
     }
 }
 
