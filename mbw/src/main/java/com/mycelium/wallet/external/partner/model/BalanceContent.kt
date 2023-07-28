b82f550b614b0cf924a5ad9c4a8fe5e98488f05e
elvis <kurtnebiev-elvis4@gmail.com>
2023-07-28 13:13:28 +0500

    #rm5782

--- mbw/src/main/java/com/mycelium/wallet/external/partner/model/BalanceContent.kt
+++ mbw/src/main/java/com/mycelium/wallet/external/partner/model/BalanceContent.kt
@@ -4,7 +4,8 @@ import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 
-data class BalanceContent(@SerializedName("buy-sell-buttons") val buttons: List<BuySellButton>)
+data class BalanceContent(@SerializedName("buy-sell-buttons") val buttons: List<BuySellButton>,
+                          @SerializedName("buy-sell-quads") val quads: List<BuySellButton>)
 
 data class BuySellButton(val name: String?,
                          val iconUrl: String?,
