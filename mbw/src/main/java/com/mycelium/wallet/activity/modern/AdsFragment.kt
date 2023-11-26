7cd4d307f85f2634a38c4cd368210e1a2d9fae5c
elvis <kurtnebiev-elvis4@gmail.com>
2023-11-26 14:18:13 +0100

    add close button to ads tab page

--- mbw/src/main/java/com/mycelium/wallet/activity/modern/AdsFragment.kt
+++ mbw/src/main/java/com/mycelium/wallet/activity/modern/AdsFragment.kt
@@ -1,25 +1,46 @@
 package com.mycelium.wallet.activity.modern
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.mycelium.wallet.R
+import com.mycelium.wallet.databinding.FragmentMarginTradeBinding
 import com.mycelium.wallet.external.partner.model.MainMenuPage
 import com.mycelium.wallet.external.partner.startContentLink
-import kotlinx.android.synthetic.main.fragment_margin_trade.*
 
 
-class AdsFragment : Fragment(R.layout.fragment_margin_trade) {
+class AdsFragment : Fragment() {
+
+    private var binding: FragmentMarginTradeBinding? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentMarginTradeBinding.inflate(inflater, container, false).apply {
+        binding = this
+    }.root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val pageData = arguments?.get("page") as MainMenuPage?
-        Glide.with(banner)
+        binding?.banner?.run {
+            Glide.with(this)
                 .load(pageData?.imageUrl)
-                .into(banner)
-        banner.setOnClickListener {
-            startContentLink(pageData?.link)
+                .into(this)
+            setOnClickListener {
+                startContentLink(pageData?.link)
+            }
         }
+        binding?.close?.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
     }
 }
