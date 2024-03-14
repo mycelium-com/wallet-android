79f6542fab0a60c7056fd61ece36abc33a055a0a
ismurzin <i.murzin@scalablesolutions.io>
2024-03-14 14:50:46 +0400

    add mycelium host

--- mbw/src/main/java/com/mycelium/wallet/external/changelly/ChangellyRetrofitFactory.kt
+++ mbw/src/main/java/com/mycelium/wallet/external/changelly/ChangellyRetrofitFactory.kt
@@ -7,19 +7,31 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.net.ssl.SSLContext
 
 object ChangellyRetrofitFactory {
-    private const val BASE_URL = "https://api.changelly.com/v2/"
+    private const val BASE_URL = "https://changelly-viper.mycelium.com/v2/"
 
     private val userKeyPair = UserKeysManager.userSignKeys
 
-    private fun getHttpClient() =
-        OkHttpClient.Builder().apply {
+    private fun getHttpClient(): OkHttpClient {
+        val sslContext = SSLContext.getInstance("TLSv1.3")
+        sslContext.init(null, null, null)
+        return OkHttpClient.Builder().apply {
+            // sslSocketFactory uses system defaults X509TrustManager, so deprecation suppressed
+            // referring to sslSocketFactory(SSLSocketFactory, X509TrustManager) docs:
+            /**
+             * Most applications should not call this method, and instead use the system defaults.
+             * Those classes include special optimizations that can be lost
+             * if the implementations are decorated.
+             */
+            @Suppress("DEPRECATION") sslSocketFactory(sslContext.socketFactory)
             addInterceptor(ChangellyInterceptor())
             addInterceptor(DigitalSignatureInterceptor(userKeyPair))
             if (!BuildConfig.DEBUG) return@apply
             addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
         }.build()
+    }
 
 
     val api: ChangellyAPIService =
--- mbw/src/main/java/com/mycelium/wallet/external/vip/VipRetrofitFactory.kt
+++ mbw/src/main/java/com/mycelium/wallet/external/vip/VipRetrofitFactory.kt
@@ -7,25 +7,38 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.net.ssl.SSLContext
 
 class VipRetrofitFactory {
     private companion object {
-        const val BASE_URL = ""
+        const val BASE_URL = "https://changelly-viper.mycelium.com"
     }
 
     private val userKeyPair = UserKeysManager.userSignKeys
 
-    private val httpClient = OkHttpClient.Builder()
-        .apply {
-            addInterceptor(DigitalSignatureInterceptor(userKeyPair))
-            if (!BuildConfig.DEBUG) return@apply
-            addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-        }.build()
+    private fun getHttpClient(): OkHttpClient {
+        val sslContext = SSLContext.getInstance("TLSv1.3")
+        sslContext.init(null, null, null)
+        return OkHttpClient.Builder()
+            .apply {
+                // sslSocketFactory uses system defaults X509TrustManager, so deprecation suppressed
+                // referring to sslSocketFactory(SSLSocketFactory, X509TrustManager) docs:
+                /**
+                 * Most applications should not call this method, and instead use the system defaults.
+                 * Those classes include special optimizations that can be lost
+                 * if the implementations are decorated.
+                 */
+                @Suppress("DEPRECATION") sslSocketFactory(sslContext.socketFactory)
+                addInterceptor(DigitalSignatureInterceptor(userKeyPair))
+                if (!BuildConfig.DEBUG) return@apply
+                addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            }.build()
+    }
 
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("$BASE_URL/api/v1/vip-codes/")
         .addConverterFactory(GsonConverterFactory.create())
-        .client(httpClient)
+        .client(getHttpClient())
         .build()
 
     fun createApi(): VipAPI = retrofit.create(VipAPI::class.java)
