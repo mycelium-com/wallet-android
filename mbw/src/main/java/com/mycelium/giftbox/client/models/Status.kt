c5a4df5d0452647114924b989287ff0b97538ab5
elvis <kurtnebiev-elvis4@gmail.com>
2024-12-02 15:07:35 +0100

    many different status handling

--- mbw/src/main/java/com/mycelium/giftbox/client/models/Status.kt
+++ mbw/src/main/java/com/mycelium/giftbox/client/models/Status.kt
@@ -1,19 +1,55 @@
 package com.mycelium.giftbox.client.models
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 
 /**
  * Order status
  * Values: sUCCESS,eRROR,pROCESSING
  */
 
-enum class Status(val value: String) {
-    @JsonProperty(value = "pending_payment")
-    PENDING("pending"),
-    @JsonProperty(value = "card_issued")
-    sUCCESS("SUCCESS"),
-    @JsonProperty(value = "failed")
-    eRROR("ERROR"),
-    @JsonProperty(value = "EXPIRED")
-    EXPIRED("EXPIRED");
+
+class CustomStatusDeserializer : JsonDeserializer<Status>() {
+    override fun deserialize(
+        p: JsonParser,
+        ctxt: DeserializationContext?
+    ): Status? =
+        when (p.text) {
+            "pending", "pending_payment",
+            "payment_confirmed", "payment_pending_confirmation",
+            "card_requested" ->
+                Status.PENDING
+
+            "underpaid", "overpaid" ->
+                Status.eRROR
+
+            "card_issued" -> Status.sUCCESS
+            "failed" -> Status.eRROR
+            "cancelled" -> Status.CANCELLED
+            else -> null
+        }
 }
+
+@JsonDeserialize(using = CustomStatusDeserializer::class)
+enum class Status(val value: String) {
+    //    @JsonProperty(value = "pending_payment")
+    PENDING("pending"),
+
+    //    @JsonProperty(value = "card_issued")
+    sUCCESS("SUCCESS"),
+
+    //    @JsonProperty(value = "failed")
+    eRROR("ERROR"),
+
+    CANCELLED("CANCELLED"),
+
+    //    @JsonProperty(value = "EXPIRED")
+    EXPIRED("EXPIRED");
+
+}
+
