0cb1d7baabcbe5bc4c6e884c4a1288ddb4878c75
elvis <kurtnebiev-elvis4@gmail.com>
2024-12-17 12:39:36 +0000

    add boost gap limit to menu remove fio from start accounts

--- bitlib/src/main/java/com/mrd/bitlib/crypto/HDKeyMagic.kt
+++ bitlib/src/main/java/com/mrd/bitlib/crypto/HDKeyMagic.kt
@@ -2,28 +2,33 @@ package com.mrd.bitlib.crypto
 
 import com.google.common.collect.BiMap
 import com.google.common.collect.HashBiMap
+import com.mrd.bitlib.crypto.BipDerivationType
 import com.mrd.bitlib.util.HexUtils.toBytes
 
 enum class HDKeyMagic(val isPrivate: Boolean, val isProdnet: Boolean, val typeToMagicMap: BiMap<BipDerivationType, ByteArray>) {
     PRODNET_PUBLIC(false, true,
             mapOf(BipDerivationType.BIP44 to toBytes("04 88 B2 1E"),    // xpub
                     BipDerivationType.BIP49 to toBytes("04 9d 7c b2"),    // ypub
-                    BipDerivationType.BIP84 to toBytes("04 b2 47 46")    // zpub
+                    BipDerivationType.BIP84 to toBytes("04 b2 47 46"),    // zpub
+                    BipDerivationType.BIP86 to toBytes("00 00 00 00")
                     ).toBiMap()),
     TESTNET_PUBLIC(false, false,
             mapOf(BipDerivationType.BIP44 to toBytes("04 35 87 CF"),    // tpub
                     BipDerivationType.BIP49 to toBytes("04 4a 52 62"),    // upub
-                    BipDerivationType.BIP84 to toBytes("04 5f 1c f6")    // vpub
+                    BipDerivationType.BIP84 to toBytes("04 5f 1c f6"),    // vpub
+                    BipDerivationType.BIP86 to toBytes("00 00 00 00")
                     ).toBiMap()),
     PRODNET_PRIVATE(true, true,
             mapOf(BipDerivationType.BIP44 to toBytes("04 88 AD E4"),    // xprv
                     BipDerivationType.BIP49 to toBytes("04 9d 78 78"),    // yprv
-                    BipDerivationType.BIP84 to toBytes("04 b2 43 0c")    // zprv
+                    BipDerivationType.BIP84 to toBytes("04 b2 43 0c"),    // zprv
+                    BipDerivationType.BIP86 to toBytes("00 00 00 00")
                     ).toBiMap()),
     TESTNET_PRIVATE(true, false,
             mapOf(BipDerivationType.BIP44 to toBytes("04 35 83 94"),    // tprv
                     BipDerivationType.BIP49 to toBytes("04 4a 4e 28"),    // uprv
-                    BipDerivationType.BIP84 to toBytes("04 5f 18 bc")  // vprv
+                    BipDerivationType.BIP84 to toBytes("04 5f 18 bc"),  // vprv
+                    BipDerivationType.BIP86 to toBytes("00 00 00 00")
                     ).toBiMap())
 }
 
