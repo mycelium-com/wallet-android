19f34888033cfa54ab365c0d521e8612f5d6e159
elvis <kurtnebiev-elvis4@gmail.com>
2025-05-07 17:40:41 +0200

    fix texts

--- mbw/src/main/java/com/mycelium/wallet/external/changelly/ExchangeKeys.kt
+++ mbw/src/main/java/com/mycelium/wallet/external/changelly/ExchangeKeys.kt
@@ -3,6 +3,6 @@ package com.mycelium.wallet.external.changelly
 object ExchangeKeys {
     const val VIPER_BASE_URL = "https://changelly-viper.mycelium.com/v2/"
 
-    const val EXCHANGE_BASE_URL = "http://mbwexchange.com:8080/api/v1/"
+    const val EXCHANGE_BASE_URL = "https://mbwexchange.com/api/v1/"
 //    const val EXCHANGE_BASE_URL = "https://api.changelly.com/v2/"
 }
